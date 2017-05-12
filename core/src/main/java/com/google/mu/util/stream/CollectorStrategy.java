d9159b33660b004951fe0e40550a56f99da9a5b0
fluentfuture <yujige@gmail.com>
2017-05-12 00:07:40 -0500

    javadoc for CollectorStrategy

--- core/src/main/java/com/google/mu/util/stream/CollectorStrategy.java
+++ core/src/main/java/com/google/mu/util/stream/CollectorStrategy.java
@@ -4,9 +4,9 @@ import java.util.Collection;
 import java.util.stream.Collector;
 
 /**
- * Strategy of collectors to use for APIs that need to parameterize the type of collections to use
- * for internal element types. For example:
- * {@code Collectors::toList} and {@code ImmutableList::toImmutableList} are two different strategies.
+ * APIs that need to parameterize the type of collections to use for internal element types
+ * can accept a {@code CollectorStrategy} as parameter. For example:
+ * {@code theApi(Collectors::toList)} or {@code theApi(ImmutableList::toImmutableList)}.
  */
 @FunctionalInterface
 public interface CollectorStrategy {
