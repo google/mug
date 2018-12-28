058a2f8639d48122b2378da3f56fd931216849c6
fluentfuture <yujige@gmail.com>
2018-12-28 14:59:05 -0800

    Javadoc regenerated

--- core/src/main/java/com/google/mu/util/Optionals.java
+++ core/src/main/java/com/google/mu/util/Optionals.java
@@ -12,8 +12,6 @@ public final class Optionals {
 
   /**
    * Invokes {@code consumer} if both {@code left} and {@code right} are present.
-   *
-   * @throws E if consumer throws
    */
   public static <A, B, E extends Throwable> void ifPresent(
       Optional<A> left, Optional<B> right, CheckedBiConsumer<? super A, ? super B, E> consumer)
@@ -30,8 +28,6 @@ public final class Optionals {
    * Maps {@code left} and {@code right} using {@code mapper} if both are present.
    * Returns an {@link Optional} wrapping the result of {@code mapper} if non-null, or else returns
    * {@code Optional.empty()}.
-   *
-   * @throws E if mapper throws
    */
   public static <A, B, R, E extends Throwable> Optional<R> map(
       Optional<A> left, Optional<B> right, CheckedBiFunction<? super A, ? super B, ? extends R, E> mapper)
@@ -51,7 +47,6 @@ public final class Optionals {
    * is empty.
    *
    * @throws NullPointerException if {@code mapper} returns null
-   * @throws E if mapper throws
    */
   public static <A, B, R, E extends Throwable> Optional<R> flatMap(
       Optional<A> left, Optional<B> right,
