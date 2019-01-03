04369916a8ce4810d296e2ac4ff87babe7c9e38b
fluentfuture <yujige@gmail.com>
2019-01-02 23:21:01 -0800

    Add new files

---
+++ core/src/main/java/com/google/mu/util/Conditional.java
@@ -0,0 +1,39 @@
+package com.google.mu.util;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.mu.function.CheckedRunnable;
+import com.google.mu.function.CheckedSupplier;
+
+/**
+ * Provides instances of {@link Premise}.
+ * 
+ * @since 1.14
+ */
+enum Conditional implements Premise {
+  TRUE {
+    @Override public <E extends Throwable> void orElse(CheckedRunnable<E> block) {
+      requireNonNull(block);
+    }
+    @Override public <E extends Throwable> Premise or(CheckedSupplier<? extends Premise, E> alternative) {
+      requireNonNull(alternative);
+      return this;
+    }
+    @Override public Premise or(Premise alternative) {
+      requireNonNull(alternative);
+      return this;
+    }
+  },
+  FALSE {
+    @Override public <E extends Throwable> void orElse(CheckedRunnable<E> block) throws E {
+      block.run();
+    }
+    @Override public <E extends Throwable> Premise or(CheckedSupplier<? extends Premise, E> alternative)
+        throws E {
+      return alternative.get();
+    }
+    @Override public Premise or(Premise alternative) {
+      return requireNonNull(alternative);
+    }
+  }
+}
