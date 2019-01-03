7f66dc5254aad99f5fb74a2aa3846360f748d3a6
fluentfuture <yujige@gmail.com>
2019-01-02 23:04:49 -0800

    ifPresent() returning Premise

--- core/src/main/java/com/google/mu/util/Optionals.java
+++ core/src/main/java/com/google/mu/util/Optionals.java
@@ -6,12 +6,46 @@ import java.util.Optional;
 
 import com.google.mu.function.CheckedBiConsumer;
 import com.google.mu.function.CheckedBiFunction;
+import com.google.mu.function.CheckedConsumer;
 
-/** Utilities pertaining to {@link Optional}. */
+/**
+ * Utilities pertaining to {@link Optional}.
+ *
+ * @since 1.13
+ */
 public final class Optionals {
 
-  /** Invokes {@code consumer} if both {@code left} and {@code right} are present. */
-  public static <A, B, E extends Throwable> void ifPresent(
+  /**
+   * Invokes {@code consumer} if {@code optional} is present. Returns a {@code Premise}
+   * object to allow {@link Premise#orElse orElse()} and friends to be chained. For example: <pre>
+   *   ifPresent(optionalStory, Story::tell).orElse(() -> print("no story"));
+   * </pre>
+   *
+   * <p>This method is very similar to JDK {@link Optional#ifPresent} with two differences: <ol>
+   * <li>{@code ifPresent(optionalStory, Story::tell)} reads with "if" at the beginning, which may read
+   *     somewhat more natural.
+   * <li>{@code orElse()} is chained fluently, compared to {@link Optional#ifPresentOrElse}.
+   * </ol>
+   */
+  public static <T, E extends Throwable> Premise ifPresent(
+      Optional<T> optional, CheckedConsumer<? super T, E> consumer) throws E {
+    requireNonNull(optional);
+    requireNonNull(consumer);
+    if (optional.isPresent()) {
+      consumer.accept(optional.get());
+      return Conditional.TRUE;
+    } else {
+      return Conditional.FALSE;
+    }
+  }
+
+  /**
+   * Invokes {@code consumer} if both {@code left} and {@code right} are present. Returns a {@code Premise}
+   * object to allow {@link Premise#orElse orElse()} and friends to be chained. For example: <pre>
+   *   ifPresent(when, where, Story::tell).orElse(() -> print("no story"));
+   * </pre>
+   */
+  public static <A, B, E extends Throwable> Premise ifPresent(
       Optional<A> left, Optional<B> right, CheckedBiConsumer<? super A, ? super B, E> consumer)
       throws E {
     requireNonNull(left);
@@ -19,6 +53,9 @@ public final class Optionals {
     requireNonNull(consumer);
     if (left.isPresent() && right.isPresent()) {
       consumer.accept(left.get(), right.get());
+      return Conditional.TRUE;
+    } else {
+      return Conditional.FALSE;
     }
   }
 
--- core/src/test/java/com/google/mu/util/OptionalsTest.java
+++ core/src/test/java/com/google/mu/util/OptionalsTest.java
@@ -1,12 +1,14 @@
 package com.google.mu.util;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.mu.util.Optionals.ifPresent;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,28 +17,109 @@ import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.google.common.testing.ClassSanityTester;
 import com.google.common.testing.NullPointerTester;
 
 @RunWith(JUnit4.class)
 public class OptionalsTest {
   @Mock private BiAction action;
+  @Mock private Consumer<Object> consumer;
+  @Mock private Runnable otherwise;
 
   @Before public void setUpMocks() {
     MockitoAnnotations.initMocks(this);
   }
 
+  @Test public void ifPresent_or_firstIsAbsent_secondSupplierIsPresent() {
+    ifPresent(Optional.empty(), consumer::accept)
+        .or(() -> ifPresent(Optional.of("left"), Optional.of("right"), action::run))
+        .orElse(otherwise::run);
+    verify(consumer, never()).accept(any());
+    verify(action).run("left", "right");
+    verify(otherwise, never()).run();
+  }
+
+  @Test public void ifPresent_or_firstIsAbsent_secondIsPresent() {
+    ifPresent(Optional.empty(), consumer::accept)
+        .or(ifPresent(Optional.of("left"), Optional.of("right"), action::run))
+        .orElse(otherwise::run);
+    verify(consumer, never()).accept(any());
+    verify(otherwise, never()).run();
+  }
+
+  @Test public void ifPresent_or_firstIsAbsent_secondSupplierIsAbsent() {
+    ifPresent(Optional.empty(), consumer::accept)
+        .or(() -> ifPresent(Optional.empty(), Optional.of("right"), action::run))
+        .orElse(otherwise::run);
+    verify(consumer, never()).accept(any());
+    verify(action, never()).run(any(), any());
+    verify(otherwise).run();
+  }
+
+  @Test public void ifPresent_or_firstIsAbsent_secondIsAbsent() {
+    ifPresent(Optional.empty(), consumer::accept)
+        .or(ifPresent(Optional.empty(), Optional.of("right"), action::run))
+        .orElse(otherwise::run);
+    verify(consumer, never()).accept(any());
+    verify(otherwise).run();
+  }
+
+  @Test public void ifPresent_or_firstIsPresent_secondSupplierIsAbsent() {
+    ifPresent(Optional.of("foo"), consumer::accept)
+        .or(() -> ifPresent(Optional.empty(), Optional.of("right"), action::run))
+        .orElse(otherwise::run);
+    verify(consumer).accept("foo");
+    verify(action, never()).run(any(), any());
+    verify(otherwise, never()).run();
+  }
+
+  @Test public void ifPresent_or_firstIsPresent_secondIsAbsent() {
+    ifPresent(Optional.of("foo"), consumer::accept)
+        .or(ifPresent(Optional.empty(), Optional.of("right"), action::run))
+        .orElse(otherwise::run);
+    verify(consumer).accept("foo");
+    verify(otherwise, never()).run();
+  }
+
+  @Test public void ifPresent_or_firstIsPresent_secondSupplierIsPresent() {
+    ifPresent(Optional.of("foo"), consumer::accept)
+        .or(() -> ifPresent(Optional.of("left"), Optional.of("right"), action::run))
+        .orElse(otherwise::run);
+    verify(consumer).accept("foo");
+    verify(action, never()).run(any(), any());
+    verify(otherwise, never()).run();
+  }
+
+  @Test public void ifPresent_or_firstIsPresent_secondIsPresent() {
+    ifPresent(Optional.of("foo"), consumer::accept)
+        .or(ifPresent(Optional.of("left"), Optional.of("right"), action::run))
+        .orElse(otherwise::run);
+    verify(consumer).accept("foo");
+    verify(otherwise, never()).run();
+  }
+
+  @Test public void ifPresent_optionalIsEmpty() {
+    ifPresent(Optional.empty(), consumer::accept);
+    verify(consumer, never()).accept(any());
+  }
+
+  @Test public void ifPresent_optionalIsPresent() {
+    ifPresent(Optional.of("foo"), consumer::accept);
+    verify(consumer).accept("foo");
+  }
+
   @Test public void ifPresent_leftIsEmpty() {
-    Optionals.ifPresent(Optional.empty(), Optional.of("bar"), action::run);
+    ifPresent(Optional.empty(), Optional.of("bar"), action::run);
     verify(action, never()).run(any(), any());
   }
 
   @Test public void ifPresent_rightIsEmpty() {
-    Optionals.ifPresent(Optional.of("foo"), Optional.empty(), action::run);
+    ifPresent(Optional.of("foo"), Optional.empty(), action::run);
     verify(action, never()).run(any(), any());
   }
 
   @Test public void ifPresent_bothPresent() {
-    Optionals.ifPresent(Optional.of("foo"), Optional.of("bar"), action::run);
+    ifPresent(Optional.of("foo"), Optional.of("bar"), action::run);
     verify(action).run("foo", "bar");
   }
 
@@ -82,13 +165,22 @@ public class OptionalsTest {
 
   @Test public void flatMap_bothPresent_mapperReturnsNonNull() {
     assertThat(Optionals.flatMap(Optional.of("foo"), Optional.of("bar"), (a, b) -> Optional.of(a + b)))
-        .isEqualTo(Optional.ofNullable("foobar"));
+        .isEqualTo(Optional.of("foobar"));
   }
 
   @Test public void testNulls() throws Exception {
     new NullPointerTester()
         .setDefault(Optional.class, Optional.empty())
         .testAllPublicStaticMethods(Optionals.class);
+    new NullPointerTester()
+        .setDefault(Optional.class, Optional.of("foo"))
+        .testAllPublicStaticMethods(Optionals.class);
+    new ClassSanityTester()
+        .setDefault(Optional.class, Optional.empty())
+        .forAllPublicStaticMethods(Optionals.class).testNulls();
+    new ClassSanityTester()
+        .setDefault(Optional.class, Optional.of("foo"))
+        .forAllPublicStaticMethods(Optionals.class).testNulls();
   }
   
   private interface BiAction {
