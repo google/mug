3026a490b5babc9157b74ece829b38ef5f4ec9e5
Ben Yu <yujige@gmail.com>
2020-01-04 11:11:00 -0800

    histogram by user:

--- core/src/main/java/com/google/mu/util/stream/BiCollectors.java
+++ core/src/main/java/com/google/mu/util/stream/BiCollectors.java
@@ -478,8 +478,8 @@ public final class BiCollectors {
    *       .collect(groupingBy(Event::cell, groupingBy(Event::hour, counting())))
    *       .collect(toHistogram());
    *
-   *   ImmutableList<HistogramBucket> byPeerUser = events.stream()
-   *       .collect(groupingBy(Event::peerUser, groupingBy(Event::hour, counting())))
+   *   ImmutableList<HistogramBucket> byUser = events.stream()
+   *       .collect(groupingBy(Event::user, groupingBy(Event::hour, counting())))
    *       .collect(toHistogram());
    *
    *   private static BiCollector<Object, BiStream<?, Long>, List<HistogramBucket>> toHistogram() {
