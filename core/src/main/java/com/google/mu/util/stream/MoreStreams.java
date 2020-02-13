dc371c02bfee3c781d9ed922db42935a8fde5f91
Ben Yu <yujige@gmail.com>
2020-02-12 19:16:32 -0800

    unused import

--- core/src/main/java/com/google/mu/util/stream/MoreStreams.java
+++ core/src/main/java/com/google/mu/util/stream/MoreStreams.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Spliterator;
-import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collector;
