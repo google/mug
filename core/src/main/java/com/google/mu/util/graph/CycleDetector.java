b958f186ebdb49dcc0e30a2f510b8f58209ea5eb
Ben Yu <yujige@gmail.com>
2020-06-09 23:47:49 -0700

    detectCyclesFrom()

--- core/src/main/java/com/google/mu/util/graph/CycleDetector.java
+++ core/src/main/java/com/google/mu/util/graph/CycleDetector.java
@@ -17,6 +17,8 @@ package com.google.mu.util.graph;
 import static com.google.mu.util.graph.Walker.nonNullList;
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -53,7 +55,6 @@ public final class CycleDetector<N> {
    * numbers for instance).
    *
    * @param startNode the node to start walking the graph.
-   * @param findSuccessors the function to find successors of any given node.
    * @return The stream of nodes starting from the first of {@code startNodes} that leads to a
    *         cycle, ending with nodes along a cyclic path. The last node will also be the starting
    *         point of the cycle. That is, if {@code A} and {@code B} form a cycle, the stream ends
@@ -71,32 +72,71 @@ public final class CycleDetector<N> {
    * numbers for instance).
    *
    * @param startNodes the nodes to start walking the graph.
-   * @param findSuccessors the function to find successors of any given node.
    * @return The stream of nodes starting from the first of {@code startNodes} that leads to a
    *         cycle, ending with nodes along a cyclic path. The last node will also be the starting
    *         point of the cycle. That is, if {@code A} and {@code B} form a cycle, the stream ends
    *         with {@code A -> B -> A}. If there is no cycle, {@link Stream#empty} is returned.
    */
   public Stream<N> detectCycleFrom(Iterable<? extends N> startNodes) {
+    return detectCyclesFrom(startNodes).findFirst().orElse(Stream.empty());
+  }
+
+  /**
+   * Walking from {@code startNodes}, detects all simple cycles in the graph topology.
+   *
+   * @param startNodes the nodes to start walking the graph.
+   * @return A lazy stream of cycles each starting from one of {@code startNodes} that leads to the
+   *         cycle, ending with nodes along the cyclic path. The last node will also be the starting
+   *         point of the cycle. That is, if {@code A} and {@code B} form a cycle, the stream ends
+   *         with {@code A -> B -> A}. If there is no cycle, {@link Stream#empty} is returned.
+   */
+  @SafeVarargs
+  public final Stream<Stream<N>> detectCyclesFrom(N... startNodes) {
+    return detectCyclesFrom(nonNullList(startNodes));
+  }
+
+  /**
+   * Walking from {@code startNodes}, detects all simple cycles in the graph topology.
+   *
+   * @param startNodes the nodes to start walking the graph.
+   * @return A lazy stream of cycles each starting from one of {@code startNodes} that leads to the
+   *         cycle, ending with nodes along the cyclic path. The last node will also be the starting
+   *         point of the cycle. That is, if {@code A} and {@code B} form a cycle, the stream ends
+   *         with {@code A -> B -> A}. If there is no cycle, {@link Stream#empty} is returned.
+   */
+  public Stream<Stream<N>> detectCyclesFrom(Iterable<? extends N> startNodes) {
     AtomicReference<N> cyclic = new AtomicReference<>();
-    Set<N> seen = new HashSet<>();
-    LinkedHashSet<N> stack = new LinkedHashSet<>();
+    Deque<N> enclosingCycles = new ArrayDeque<>();
+    Set<N> deadEnds = new HashSet<>();  // Always a superset of `currentPath`.
+    LinkedHashSet<N> currentPath = new LinkedHashSet<>();
     Walker<N> walker = Walker.inGraph(findSuccessors, new Predicate<N>() {
       @Override public boolean test(N node) {
-        boolean newNode = seen.add(node);
+        boolean newNode = deadEnds.add(node);
         if (newNode) {
-          stack.add(node);
-        } else if (stack.contains(node)) {
-          cyclic.compareAndSet(null, node);  // stick to the first finding
+          currentPath.add(node);
+        } else if (currentPath.contains(node)) {  // A cycle's found!
+          cyclic.compareAndSet(null, node);
+          enclosingCycles.push(node);
         }
         return newNode;
       }
     });
     return walker.postOrderFrom(startNodes)
-        .peek(stack::remove)
+        .peek(n -> {
+          currentPath.remove(n);
+          if (n.equals(enclosingCycles.peek())) {
+            // exiting a cycle.
+            // In case of a self-cycle, we immediately pop it because nothing is enclosed.
+            // `cyclic` still points to this cyclic node for the later map() call to use.
+            enclosingCycles.pop();
+          }
+          if (!enclosingCycles.isEmpty()) {
+            // If we are in a cycle, we want to come back in again in case it forms another cycle
+            // from a different path. Otherwise, keep it as a dead end.
+            deadEnds.remove(n);
+          }
+        })
         .filter(n -> cyclic.get() != null)
-        .findFirst()
-        .map(last -> Stream.concat(stack.stream(), Stream.of(last, cyclic.get())))
-        .orElse(Stream.empty());
+        .map(last -> Stream.concat(currentPath.stream(), Stream.of(last, cyclic.getAndSet(null))));
   }
 }
