0aa3adaae6ff05a887a6986bbd34c60287a38d01
Ben Yu <yujige@gmail.com>
2020-06-12 12:56:56 -0700

    Selection.allIfEmpty()

--- core/src/main/java/com/google/mu/util/Selection.java
+++ core/src/main/java/com/google/mu/util/Selection.java
@@ -15,10 +15,13 @@
 package com.google.mu.util;
 
 import static com.google.mu.util.InternalCollectors.toImmutableSet;
+import static com.google.mu.util.stream.MoreStreams.whileNotNull;
+import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.reducing;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collector;
@@ -73,6 +76,21 @@ public interface Selection<T> {
   @SafeVarargs
   static <T> Selection<T> only(T... choices) {
     return Arrays.stream(choices).collect(toSelection());
+  }
+
+  /**
+   * Converts from legacy code where null or an empty {@code Iterable} means <em>all</em> to {@code
+   * Selection}. After converted, user code of the {@code Selection} no longer need special handling
+   * of the <em>empty</em> or <em>null</em> case.
+   */
+  static <T> Selection<T> allIfEmpty(Iterable<? extends T> choices) {
+    if (choices == null) {
+      return all();
+    }
+    Iterator<? extends T> it = choices.iterator();
+    return it.hasNext()
+        ? whileNotNull(() -> it.hasNext() ? requireNonNull(it.next()) : null).collect(toSelection())
+        : all();
   }
 
   /** Returns a collector that collects input elements into a limited selection. */
