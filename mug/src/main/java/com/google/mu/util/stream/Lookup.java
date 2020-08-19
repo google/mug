b9302ea8d7332dfa85e9f6f86e04724eb1be6185
Ben Yu <yujige@gmail.com>
2020-08-19 08:58:12 -0700

    Lookup

---
+++ mug/src/main/java/com/google/mu/util/stream/Lookup.java
@@ -0,0 +1,113 @@
+/*****************************************************************************
+ * ------------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License");           *
+ * you may not use this file except in compliance with the License.          *
+ * You may obtain a copy of the License at                                   *
+ *                                                                           *
+ * http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ *****************************************************************************/
+package com.google.mu.util.stream;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+/**
+ * This class aims to simplify the common "lookup-then-filter" boilerplate in stream chains:
+ *
+ * <pre>{@code
+ * Map<K, V> map = ...;
+ * collection.stream()
+ *     .map(map::get)
+ *     .filter(v -. v != null)
+ *     ...;
+ * }</pre>
+ *
+ * <p>Using {@code Lookup}, it can be simplified to:
+ *
+ * <pre>{@code
+ * Map<K, V> map = ...;
+ * collection.stream()
+ *     .flatMap(Lookup.in(map))
+ *     ...;
+ * }</pre>
+ *
+ * <p>When used in a {@code Collector}:
+ *
+ * <pre>{@code
+ * ImmutableMap<K, V> map = ...;
+ * return Collectors.flatMapping(Lookup.in(map), downstream);
+ * }</pre>
+ *
+ * <p>In a slightly different variant, you may need to retain both the original element and the
+ * looked-up result, you can keep them in a {@link BiStream} by using the {@link #by} method.
+ * For example:
+ *
+ * <pre>{@code
+ * Map<StudentId, Score> testResults = ...;
+ * BiStream<Student, Score> studentsWithScore =
+ *     BiStream.concat(students.stream().map(Lookup.in(testResults).by(Student::id)));
+ * }</pre>
+ *
+ * Or, it can be composed with {@link BiStream#concatenating} to make syntax more fluent:
+ *
+ * <pre>{@code
+ * import static com.google.mu.util.stream.BiStream.concatenating;
+ *
+ * Map<StudentId, Score> testResults = ...;
+ * BiStream<Student, Score> studentsWithScore =
+ *     students.stream()
+ *         .collect(concatenating(Lookup.in(testResults).by(Student::id)));
+ * }</pre>
+ *
+ * @since 4.7
+ */
+public final class Lookup<K, V> implements Function<K, Stream<V>> {
+  private final Map<? extends K, ? extends V> map;
+
+  private Lookup(Map<? extends K, ? extends V> map) {
+    this.map = requireNonNull(map);
+  }
+
+  /**
+   * Returns an instance backed by {@code map}.
+   * The returned instance can then be passed to {@link flatMap()} for a stream.
+   */
+  public static <K, V> Lookup<K, V> in(Map<K, V> map) {
+    return new Lookup<>(map);
+  }
+
+  /**
+   * Returns an function that for any given {@code input}, applies {@code keyFunction} and then
+   * looks up the value mapped to the key from the backing {@code Map}.
+   * If a value is found, the pair of {@code (input, looked-up-value)} is returned; otherwise empty.
+   *
+   * <p>Can be composed with {@link BiStream#concat(Stream)} to create {@code BiStream}s:
+   *
+   * <pre>{@code
+   * Map<StudentId, Score> testResults = ...;
+   * BiStream<Student, Score> studentsWithScore =
+   *     BiStream.concat(students.stream().map(Lookup.in(testResults).by(Student::id)));
+   * }</pre>
+   */
+  public final <T> Function<T, BiStream<T, V>> by(Function<? super T, ? extends K> keyFunction) {
+    requireNonNull(keyFunction);
+    return input -> {
+      V value = map.get(keyFunction.apply(input));
+      return value == null ? BiStream.empty() : BiStream.of(input, value);
+    };
+  }
+
+  @Override public Stream<V> apply(K key) {
+    V value = map.get(key) ;
+    return value == null ? Stream.empty() : Stream.of(value);
+  }
+}
---
+++ mug/src/test/java/com/google/mu/util/stream/LookupTest.java
@@ -0,0 +1,63 @@
+/*****************************************************************************
+ * ------------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License");           *
+ * you may not use this file except in compliance with the License.          *
+ * You may obtain a copy of the License at                                   *
+ *                                                                           *
+ * http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ *****************************************************************************/
+package com.google.mu.util.stream;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth8.assertThat;
+import static com.google.mu.util.stream.BiStream.concatenating;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.common.testing.NullPointerTester;
+
+public class LookupTest {
+  @Test public void notFoundInMap() {
+    ImmutableMap<String, Integer> map = ImmutableMap.of("one", 1);
+    assertThat(Stream.of("two").flatMap(Lookup.in(map))).isEmpty();
+  }
+
+  @Test public void foundInMap() {
+    ImmutableMap<String, Integer> map = ImmutableMap.of("one", 1);
+    assertThat(Stream.of("one").flatMap(Lookup.in(map))).containsExactly(1);
+  }
+
+  @Test public void by_keyNotFoundInMap() {
+    ImmutableMap<String, Integer> map = ImmutableMap.of("one", 1);
+    BiStream<Entry<String, String>, Integer> concatenated =
+        Stream.of(Maps.immutableEntry("two", "dos"))
+            .collect(concatenating(Lookup.in(map).by(Map.Entry::getKey)));
+    assertThat(concatenated.toMap()).isEmpty();
+  }
+
+  @Test public void by_keyFoundInMap() {
+    ImmutableMap<String, Integer> map = ImmutableMap.of("one", 1);
+    BiStream<Map.Entry<String, String>, Integer> concatenated = BiStream.concat(
+        Stream.of(Maps.immutableEntry("two", "dos")).map(Lookup.in(map).by(Map.Entry::getKey)));
+    assertThat(concatenated.toMap()).isEmpty();
+  }
+
+  @Test public void testNulls() throws Exception {
+    new NullPointerTester().testAllPublicStaticMethods(Lookup.class);
+    new NullPointerTester()
+        .ignore(Lookup.class.getMethod("apply", Object.class))
+        .testAllPublicInstanceMethods(Lookup.in(ImmutableMap.of()));
+  }
+}
