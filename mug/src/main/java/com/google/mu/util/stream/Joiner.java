825d729f24db0664f3ad92407bbbc91ce4ec8e9d
Ben Yu <yujige@gmail.com>
2021-10-30 23:13:18 -0700

    Simplify Joiner

--- mug/src/main/java/com/google/mu/util/stream/Joiner.java
+++ mug/src/main/java/com/google/mu/util/stream/Joiner.java
@@ -28,7 +28,7 @@ import java.util.stream.Collectors;
  * Except that JDK {@code joining()} requires the inputs to be strings; while Joiner can join any input,
  * e.g. numbers.
  *
- * <p>You can also chain {@link #between} to further enclose the joined result.
+ * <p>You can also chain {@link #between} to further enclose the joined result between a pair of strings.
  * The following code joins a list of ids and their corresponding names in the format of
  * {@code "[id1:name1, id2:name2, id3:name3, ...]"}:
  * <pre>{@code
@@ -51,50 +51,26 @@ import java.util.stream.Collectors;
  *       .collect(Joiner.on(", ").between('[', ']'));
  * }</pre>
  *
- * <p>Unlike {@link com.google.common.base.Joiner}, nulls are forgiven and will be stringified
- * using {@link String#valueOf}. In addition, {@link #between} can be chained to further enclose
- * the joined result.
+ * <p>Unlike {@link com.google.common.base.Joiner}, nulls don't cause NullPointerException, instead,
+ * they are stringified using {@link String#valueOf}.
  *
  * @since 5.6
  */
 public abstract class Joiner implements Collector<Object, StringJoiner, String> {
 
-  /** Joining the components on the {@code delimiter} character */
+  /** Joining the inputs on the {@code delimiter} character */
   public static Joiner on(char delimiter) {
     return on(Character.toString(delimiter));
   }
 
-  /** Joining the components on the {@code delimiter} string */
+  /** Joining the inputs on the {@code delimiter} string */
   public static Joiner on(String delimiter) {
     requireNonNull(delimiter);
     return new Joiner() {
-      @Override
-      public Supplier<StringJoiner> supplier() {
+      @Override public Supplier<StringJoiner> supplier() {
         return () -> new StringJoiner(delimiter);
       }
-
-      @Override
-      public BiConsumer<StringJoiner, Object> accumulator() {
-        return (joiner, obj) -> joiner.add(String.valueOf(obj));
-      }
-
-      @Override
-      public BinaryOperator<StringJoiner> combiner() {
-        return StringJoiner::merge;
-      }
-
-      @Override
-      public Function<StringJoiner, String> finisher() {
-        return StringJoiner::toString;
-      }
-
-      @Override
-      public Set<Characteristics> characteristics() {
-        return Collections.emptySet();
-      }
-
-      @Override
-      public String join(Object l, Object r) {
+      @Override public String join(Object l, Object r) {
         return l + delimiter + r;
       }
     };
@@ -103,47 +79,43 @@ public abstract class Joiner implements Collector<Object, StringJoiner, String> 
   /** Joins {@code l} and {@code r} together. */
   public abstract String join(Object l, Object r);
 
-  /** Returns an instance that wraps the join result in the pair of {@code open} and {@code close}. */
-  public final Joiner between(char open, char close) {
-    return between(Character.toString(open), Character.toString(close));
+  /** Returns an instance that wraps the join result between {@code before} and {@code after}. */
+  public final Joiner between(char before, char after) {
+    return between(Character.toString(before), Character.toString(after));
   }
 
-  /** Returns an instance that wraps the join result in the pair of {@code open} and {@code close}. */
-  public final Joiner between(String open, String close) {
-    requireNonNull(open);
-    requireNonNull(close);
+  /** Returns an instance that wraps the join result between {@code before} and {@code after}. */
+  public final Joiner between(String before, String after) {
+    requireNonNull(before);
+    requireNonNull(after);
     Joiner base = this;
     return new Joiner() {
-      @Override
-      public Supplier<StringJoiner> supplier() {
+      @Override public Supplier<StringJoiner> supplier() {
         return base.supplier();
       }
-
-      @Override
-      public BiConsumer<StringJoiner, Object> accumulator() {
-        return base.accumulator();
+      @Override public Function<StringJoiner, String> finisher() {
+       return base.finisher().andThen(r -> before + r + after);
       }
-
-      @Override
-      public BinaryOperator<StringJoiner> combiner() {
-        return base.combiner();
-      }
-
-      @Override
-      public Function<StringJoiner, String> finisher() {
-       return base.finisher().andThen(r -> open + r + close);
-      }
-
-      @Override
-      public Set<Characteristics> characteristics() {
-        return Collections.emptySet();
-      }
-
-      @Override
-      public String join(Object l, Object r) {
-        return open + base.join(l, r) + close;
+      @Override public String join(Object l, Object r) {
+        return before + base.join(l, r) + after;
       }
     };
+  }
+
+  @Override public BiConsumer<StringJoiner, Object> accumulator() {
+    return (joiner, obj) -> joiner.add(String.valueOf(obj));
+  }
+
+  @Override public BinaryOperator<StringJoiner> combiner() {
+    return StringJoiner::merge;
+  }
+
+  @Override public Function<StringJoiner, String> finisher() {
+    return StringJoiner::toString;
+  }
+
+  @Override public Set<Characteristics> characteristics() {
+    return Collections.emptySet();
   }
 
   Joiner() {}
