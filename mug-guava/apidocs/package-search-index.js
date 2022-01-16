81d2bed2e64d79d00d0f675f13eacd901e78d6d2
Ben Yu <yujige@gmail.com>
2022-01-15 16:11:54 -0800

    Javadoc regenerated

--- mug-guava/apidocs/package-search-index.js
+++ mug-guava/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.google.mu.util.stream"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.google.mu.collect"},{"l":"com.google.mu.util.stream"}]
--- mug-guava/apidocs/type-search-index.js
+++ mug-guava/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.google.mu.util.stream","l":"GuavaCollectors"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.google.mu.util.stream","l":"GuavaCollectors"},{"p":"com.google.mu.collect","l":"Immutables"}]
