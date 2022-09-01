7c7dc42e94af008174c70619b292226cd7404e5f
Ben Yu <yujige@gmail.com>
2022-08-31 17:06:50 -0700

    InsertionPoint javadoc

--- mug-guava/src/main/java/com/google/mu/util/InsertionPoint.java
+++ mug-guava/src/main/java/com/google/mu/util/InsertionPoint.java
@@ -17,31 +17,30 @@ import com.google.common.collect.Range;
 import com.google.errorprone.annotations.Immutable;
 
 /**
- * An insertion point in a sequence (normally of indexes), that's either an exact value, or in
- * between two adjacent values in a discrete domain, or before or after all values of the discrete
+ * An insertion point in a discrete domain (normally of indexes), that's either an exact element, or in
+ * between two adjacent elements in a discrete domain, or before or after all elements of the discrete
  * domain.
  *
- * <p>If representing an exact point, the {@link #exact} method will return the point's value, and
- * both {@link #floor} and {@link #ceiling} return the same value.
+ * <p>If representing an exact point, {@link #exact}, {@link #floor} and {@link #ceiling} all return the element.
  *
- * <p>If it's between two values, {@link #exact} will return empty, and {@link #floor} and {@code
- * #ceiling} will return the two values respectively.
+ * <p>If it's between two elements, {@link #exact} will return empty, and {@link #floor} and {@code
+ * #ceiling} will return the two adjacent elements respectively.
  *
- * <p>If it's below all possible values, {@link #isBelowAll} will return true and {@link #floor}
- * will throw. The {@link #ceiling} method will return the min value of the domain (if present).
+ * <p>If it's below all possible elements, {@link #isBelowAll} will return true and {@link #floor}
+ * will throw. The {@link #ceiling} method will return the min element of the domain (if present).
  *
- * <p>If it's above all possible values, {@link #isAboveAll} will return true and {@link #ceiling}
- * will throw. The {@link #floor} method will return the max value of the domain (if present).
+ * <p>If it's above all possible elements, {@link #isAboveAll} will return true and {@link #ceiling}
+ * will throw. The {@link #floor} method will return the max element of the domain (if present).
  *
- * <p>Over a discrete domain with N discrete values, there are {@code 2 * N + 1} distinct insertion
- * points, including all the values, the points between each two adjacent values, the point before
- * all values and the point after all values.
+ * <p>Over a discrete domain with N discrete elements, there are {@code 2 * N + 1} distinct insertion
+ * points, including all the elements, the points between each two adjacent elements, the point before
+ * all elements and the point after all elements.
  *
- * @param <T> the domain type
+ * @param <C> the domain type
  * @since 6.4
  */
-@Immutable(containerOf = "T")
-public final class InsertionPoint<T extends Comparable<T>> implements Comparable<InsertionPoint<T>> {
+@Immutable(containerOf = "C")
+public final class InsertionPoint<C extends Comparable<C>> implements Comparable<InsertionPoint<C>> {
   @SuppressWarnings("unchecked") // Curiously recursive generics doesn't play nicely with wildcard.
   private static final Comparator<Comparable<?>> NULL_FIRST = (Comparator<Comparable<?>>) Comparator
       .nullsFirst(naturalOrder());
@@ -50,21 +49,21 @@ public final class InsertionPoint<T extends Comparable<T>> implements Comparable
   private static final Comparator<Comparable<?>> NULL_LAST = (Comparator<Comparable<?>>) Comparator
       .nullsLast(naturalOrder());
 
-  private final @Nullable T floor;
-  private final @Nullable T ceiling;
+  private final @Nullable C floor;
+  private final @Nullable C ceiling;
 
-  private InsertionPoint(@Nullable T floor, @Nullable T ceiling) {
+  private InsertionPoint(@Nullable C floor, @Nullable C ceiling) {
     this.floor = floor;
     this.ceiling = ceiling;
   }
 
   /**
-   * Returns an insertion point exactly at {@code value} such that {@link #exact},
-   * {@link #floor} and {@link #ceiling} all return the same value.
+   * Returns an insertion point exactly at {@code element} such that {@link #exact},
+   * {@link #floor} and {@link #ceiling} all return the same element.
    */
-  public static <T extends Comparable<T>> InsertionPoint<T> at(T value) {
-    checkNotNull(value);
-    return new InsertionPoint<>(value, value);
+  public static <C extends Comparable<C>> InsertionPoint<C> at(C element) {
+    checkNotNull(element);
+    return new InsertionPoint<>(element, element);
   }
 
   /**
@@ -87,10 +86,11 @@ public final class InsertionPoint<T extends Comparable<T>> implements Comparable
 
   /**
    * Returns an insertion point immediately before the given {@code ceiling} and
-   * after the previous value in the given discrete {@code domain} (if a previous
-   * value exists).
+   * after the previous element in the given discrete {@code domain} (if a previous
+   * element exists).
    */
-  public static <T extends Comparable<T>> InsertionPoint<T> before(T ceiling, DiscreteDomain<T> domain) {
+  public static <C extends Comparable<C>> InsertionPoint<C> before(
+      C ceiling, DiscreteDomain<C> domain) {
     return new InsertionPoint<>(domain.previous(ceiling), ceiling);
   }
 
@@ -112,33 +112,34 @@ public final class InsertionPoint<T extends Comparable<T>> implements Comparable
 
   /**
    * Returns an insertion point immediately after the given {@code floor} and
-   * before the next value in the given discrete {@code domain} (if a next value
+   * before the next element in the given discrete {@code domain} (if a next element
    * exists).
    */
-  public static <T extends Comparable<T>> InsertionPoint<T> after(T floor, DiscreteDomain<T> domain) {
+  public static <C extends Comparable<C>> InsertionPoint<C> after(
+      C floor, DiscreteDomain<C> domain) {
     return new InsertionPoint<>(floor, domain.next(floor));
   }
 
   /**
    * If this represents an exact point (not between two adjacent values), returns
-   * the value of the point; else returns empty.
+   * the element at the point; else returns empty.
    */
-  public Optional<T> exact() {
-// floor == ceiling is safe because at() always uses the same reference
-// for floor and ceiling.
-// floor and ceiling will never both be nulls.
+  public Optional<C> exact() {
+    // floor == ceiling is safe because at() always uses the same reference
+    // for floor and ceiling.
+    // floor and ceiling will never both be nulls.
     return optional(floor == ceiling, floor);
   }
 
   /**
-   * Returns the floor value such that this insertion point is immediately
+   * Returns the floor element such that this insertion point is immediately
    * {@code >=} the floor.
    *
    * @throws NoSuchElementException if this represents a point below all possible
    *                                values in the discrete domain. Users can use
    *                                {@link #isBelowAll} to guard this condition.
    */
-  public T floor() {
+  public C floor() {
     if (floor == null) {
       throw new NoSuchElementException("InsertionPoint " + this + " has no floor");
     }
@@ -146,14 +147,14 @@ public final class InsertionPoint<T extends Comparable<T>> implements Comparable
   }
 
   /**
-   * Returns the ceiling value such that this insertion point is immediately
+   * Returns the ceiling element such that this insertion point is immediately
    * {@code <=} the ceiling.
    *
    * @throws NoSuchElementException if this represents a point above all possible
    *                                values in the discrete domain. Users can use
    *                                {@link #isAboveAll} to guard this condition.
    */
-  public T ceiling() {
+  public C ceiling() {
     if (ceiling == null) {
       throw new NoSuchElementException("InsertionPoint " + this + " has no ceiling");
     }
@@ -161,21 +162,21 @@ public final class InsertionPoint<T extends Comparable<T>> implements Comparable
   }
 
   /**
-   * Returns true if this is a point above the max possible value in the domain.
+   * Returns true if this is a point above the max possible element in the domain.
    */
   public boolean isAboveAll() {
     return ceiling == null;
   }
 
   /**
-   * Returns true if this is a point below the min possible value in the domain.
+   * Returns true if this is a point below the min possible element in the domain.
    */
   public boolean isBelowAll() {
     return floor == null;
   }
 
   @Override
-  public int compareTo(InsertionPoint<T> that) {
+  public int compareTo(InsertionPoint<C> that) {
     return ComparisonChain.start().compare(floor, that.floor, NULL_FIRST).compare(ceiling, that.ceiling, NULL_LAST)
         .result();
   }
