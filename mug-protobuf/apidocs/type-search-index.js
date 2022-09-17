0408f1930d0a35004201d9627831c838e06aacf5
Ben Yu <yujige@gmail.com>
2022-09-16 22:05:09 -0700

    Javadoc regenerated

--- mug-protobuf/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.google.mu.protobuf.util","l":"MoreStructs"},{"p":"com.google.mu.protobuf.util","l":"MoreValues"},{"p":"com.google.mu.protobuf.util","l":"StructBuilder"},{"p":"com.google.mu.protobuf.util","l":"Structor"}]
