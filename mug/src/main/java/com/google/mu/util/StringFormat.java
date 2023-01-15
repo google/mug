ea6ff486d415ca39837f8ef09d113501b03c6a82
Ben Yu <yujige@gmail.com>
2023-01-15 13:47:05 -0800

    StringTemplate or StringFormat?

---
+++ mug/src/main/java/com/google/mu/util/StringFormat.java
@@ -0,0 +1,331 @@
+package com.google.mu.util;
+
+import static com.google.mu.util.InternalCollectors.toImmutableList;
+import static com.google.mu.util.Optionals.optional;
+import static com.google.mu.util.Substring.before;
+import static com.google.mu.util.Substring.first;
+import static com.google.mu.util.Substring.prefix;
+import static com.google.mu.util.Substring.suffix;
+import static com.google.mu.util.stream.MoreCollectors.asIn;
+import static java.util.Collections.unmodifiableList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import com.google.mu.function.Quarternary;
+import com.google.mu.function.Quinary;
+import com.google.mu.function.Senary;
+import com.google.mu.function.Ternary;
+import com.google.mu.util.stream.BiStream;
+
+/**
+ * A utility class to extract placeholder values from input strings based on a format string.
+ * For example:
+ *
+ * <pre>{@code
+ * return new StringFormat("To %s: %s?")
+ *     .parse(input, (recipient, question) -> ...));
+ * }</pre>
+ *
+ * <p>Placeholders can be named:
+ *
+ * <pre>{@code
+ * List<String> values =
+ *     new StringFormat("To {recipient}: {question}?", "{", "}")
+ *         .parse("To Charlie: How are you?");
+ * }</pre>
+ *
+ * <p>If you'd like to access the placeholder values by name, you can {@link BiStream#zip zip}
+ * them with {@link #placeholders}:
+ *
+ * <pre>{@code
+ * StringFormat template = new StringFormat("To {recipient}: {question}?", "{", "}");
+ * Map<String, String> values =
+ *     BiStream.zip(template.placeholders(), template.parse("To Charlie: How are you?"))
+ *         .mapKeys(Substring::Match::toString)
+ *         .toMap();
+ * }</pre>
+ *
+ * <p>Note that other than the placeholders, characters in the template are treated as literals.
+ * This makes it simpler compared to regex if your pattern is close to free-form text with
+ * characters like '.', '?', '(', '|' and what not. On the other hand, if you need to use regex
+ * modifiers and quantifiers to express complex patterns, you need a regex pattern, not a literal
+ * pattern with placeholders.
+ *
+ * <p>The {@code parse()} methods are potentially lossy reverse operations of {@link String#format}
+ * or the {@code format()} methods. Consider the format string of {@code String.format("I bought %s
+ * and %s", "apples and oranges", "chips")}, it returns {@code "I bought apples and oranges and
+ * chips"}; but the parsing code will incorrectly return {@code Map.of("{fruits}", "apples",
+ * "{snacks}", "oranges and chips")}:
+ *
+ * <pre>{@code
+ * new StringFormat("I bought {fruits} and {snacks}", "{", "}")
+ *     .parse( "I bought apples and oranges and chips", (fruit, snacks) -> ...);
+ * }</pre>
+ *
+ * This is because the parser will see the " and " substring and immediately matches {@code
+ * "apples"} as {@code fruits}, with no backtracking attempted. As such, only use this class to
+ * parse unambiguous strings. Use regex otherwise.
+ *
+ * <p>This class is immutable and pre-compiles the template format at constructor time so that the
+ * {@code parse()} and {@code format()} methods will be more efficient.
+ *
+ * @since 6.6
+ */
+public final class StringFormat {
+  private final String format;
+  private final List<Substring.Match> placeholders;
+
+  /**
+   * In the input string, a placeholder value is found from the current position until the next
+   * text literal, which includes all literal characters found in the template between the previous
+   * placeholder variable (or the BEGINNING) and the next placeholder variable (or the END).
+   */
+  private final List<String> literals;
+  private final List<Substring.Pattern> literalLocators;
+
+  /**
+   * Constructs a StringFormat. For example:
+   *
+   * <pre>{@code
+   * new StringFormat("Dear {person}, your confirmation number is {confirmation_number}")
+   * }</pre>
+   *
+   * @param format the template format with placeholders in the format of {@code "{placeholder_name}"}
+   * @throws IllegalArgumentException if {@code format} is invalid
+   *     (e.g. a placeholder immediately followed by another placeholder)
+   */
+  public StringFormat(String format) {
+    this(format, first("%s").repeatedly());
+  }
+
+  /**
+   * Returns a StringFormat for the given {@code format} with {@code placeholder}.
+   *
+   * <p>For example: <pre>{@code
+   * new StringFormat("I bought {fruits} and {snacks} at price of {price}", "{", "}")
+   *     .parse("I bought ice cream and beer at price of $15.4", (a, b, price) -> ...));
+   * }</pre>
+   */
+  public StringFormat(String format, String placeholderPrefix, String placeholderSuffix) {
+    this(format, Substring.spanningInOrder(placeholderPrefix, placeholderSuffix).repeatedly());
+  }
+
+  /**
+   * Constructs a StringFormat. By default, {@code new StringFormat(format)} assumes placeholders
+   * to be enclosed by curly braces. For example: "Hello {customer}". If you need different placeholder
+   * syntax, for example, to emulate Java's "%s", you can use:
+   *
+   * <pre>{@code new StringFormat("Hi %s, my name is %s", first("%s").repeatedly())}</pre>
+   *
+   * @param format the template format with placeholders
+   * @param placeholderVariablePattern placeholders in {@code format}.
+   *     For example: {@code first("%s").repeatedly()}.
+   * @throws IllegalArgumentException if {@code format} is invalid
+   *     (e.g. a placeholder immediately followed by another placeholder)
+   */
+  private StringFormat(String format, Substring.RepeatingPattern placeholderVariablesPattern) {
+    this.format = format;
+    this.placeholders = placeholderVariablesPattern.match(format).collect(toImmutableList());
+    List<String> literals = new ArrayList<>(placeholders.size() + 1);
+    List<Substring.Pattern> literalLocators = new ArrayList<>(literals.size());
+    extractLiteralsFromFormat(format, placeholders, literals, literalLocators);
+    this.literals = unmodifiableList(literals);
+    this.literalLocators = unmodifiableList(literalLocators);
+  }
+
+  /**
+   * Parses {@code input} and applies {@code function} with the single placeholder value
+   * in this template.
+   *
+   * <p>For example: <pre>{@code
+   * new StringFormat("Job failed (job id: {})").parse(input, jobId -> ...);
+   * }</pre>
+   *
+   * @throws IllegalArgumentException if {@code input} doesn't match the format or the template
+   *     doesn't have exactly one placeholder.
+   */
+  public <R> R parse(String input, Function<? super String, R> function) {
+    return parsePlaceholderValues(input).collect(asIn(function));
+  }
+
+  /**
+   * Parses {@code input} and applies {@code function} with the two placeholder values
+   * in this template.
+   *
+   * <p>For example: <pre>{@code
+   * new StringFormat("Job failed (job id: '{}', error code: {})")
+   *     .parse(input, (jobId, errorCode) -> ...);
+   * }</pre>
+   *
+   * @throws IllegalArgumentException if {@code input} doesn't match the format or the template
+   *     doesn't have exactly two placeholders.
+   */
+  public <R> R parse(String input, BiFunction<? super String, ? super String, R> function) {
+    return parsePlaceholderValues(input).collect(asIn(function));
+  }
+
+  /**
+   * Similar to {@link #parse(String, BiFunction}, but parses {@code input} and applies {@code
+   * function} with the <em>3</em> placeholder values in this template.
+   *
+   * <p>For example: <pre>{@code
+   * new StringFormat("Job failed (job id: '{}', error code: {}, error details: {})")
+   *     .parse(input, (jobId, errorCode, errorDetails) -> ...);
+   * }</pre>
+   *
+   * @throws IllegalArgumentException if {@code input} doesn't match the format or the template
+   *     doesn't have exactly 3 placeholders.
+   */
+  public <R> R parse(String input, Ternary<? super String,  R> function) {
+    return parsePlaceholderValues(input).collect(asIn(function));
+  }
+
+  /**
+   * Similar to {@link #parse(String, BiFunction}, but parses {@code input} and applies {@code
+   * function} with the <em>4</em> placeholder values in this template.
+   *
+   * @throws IllegalArgumentException if {@code input} doesn't match the format or the template
+   *     doesn't have exactly 4 placeholders.
+   */
+  public <R> R parse(String input, Quarternary<? super String,  R> function) {
+    return parsePlaceholderValues(input).collect(asIn(function));
+  }
+
+  /**
+   * Similar to {@link #parse(String, BiFunction}, but parses {@code input} and applies {@code
+   * function} with the <em>5</em> placeholder values in this template.
+   *
+   * @throws IllegalArgumentException if {@code input} doesn't match the format or the template
+   *     doesn't have exactly 5 placeholders.
+   */
+  public <R> R parse(String input, Quinary<? super String,  R> function) {
+    return parsePlaceholderValues(input).collect(asIn(function));
+  }
+
+  /**
+   * Similar to {@link #parse(String, BiFunction}, but parses {@code input} and applies {@code
+   * function} with the <em>6</em> placeholder values in this template.
+   *
+   * @throws IllegalArgumentException if {@code input} doesn't match the format or the template
+   *     doesn't have exactly 6 placeholders.
+   */
+  public <R> R parse(String input, Senary<? super String,  R> function) {
+    return parsePlaceholderValues(input).collect(asIn(function));
+  }
+
+  /**
+   * Parses {@code input} and extracts all placeholder values, in the same order as {@link #placeholders}.
+   *
+   * @throws IllegalArgumentException if {@code input} doesn't match the format
+   */
+  public List<String> parse(String input) {
+    return parsePlaceholderValues(input).collect(toImmutableList());
+  }
+
+  /**
+   * Matches {@code input} against the pattern.
+   *
+   * <p>Returns an immutable list of placeholder values in the same order as {@link #placeholders},
+   * upon success; otherwise returns empty.
+   *
+   * <p>The {@link Substring.Match} result type allows caller to inspect the characters around each
+   * match, or to access the raw index in the input string.
+   */
+  private Optional<List<Substring.Match>> match(String input) {
+    List<Substring.Match> builder = new ArrayList<>(placeholders.size());
+    int inputIndex = 0;
+    for (int i = 0; i < literals.size(); i++) {
+      Substring.Match placeholder = before(literalLocators.get(i)).match(input, inputIndex);
+      if (placeholder == null) return Optional.empty();
+      if (i > 0) {
+        builder.add(placeholder);
+      }
+      inputIndex = placeholder.index() + placeholder.length() + literals.get(i).length();
+    }
+    return optional(inputIndex == input.length(), unmodifiableList(builder));
+  }
+
+  /**
+   * Formats this template with the provided {@code args} for each placeholder, in the same order
+   * as {@link #placeholders}. Null arg will show up as "null" in the result string.
+   *
+   * <p>For example: <pre>{@code
+   * new StringFormat("Dear {person}, your confirmation number is {confirmation#}")
+   *     .format("customer", 12345);
+   *   => "Dear customer, your confirmation number is 12345"
+   * }</pre>
+   *
+   * @throws NullPointerException if {@code args} is null
+   * @throws IllegalArgumentException if {@code args} is longer or shorter than {@link #placeholders}.
+   */
+  @SafeVarargs
+  public final String format(Object... args) {
+    if (args.length != placeholders.size()) {
+      throw new IllegalArgumentException(
+          placeholders.size() + " format arguments expected, " + args.length + " provided.");
+    }
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < args.length; i++) {
+      builder
+          .append(literals.get(i))
+          .append(args[i]);
+    }
+    return builder.append(literals.get(placeholders.size())).toString();
+  }
+
+  /**
+   * Returns the immutable list of placeholders in this template, in occurrence order.
+   *
+   * <p>Each placeholder is-a {@link CharSequence} with extra accessors to the index in this
+   * template string. Callers can also use, for example, {@code .skip(1, 1)} to easily strip away
+   * the '{' and '}' characters around the placeholder names.
+   */
+  public List<Substring.Match> placeholders() {
+    return placeholders;
+  }
+
+  /** Returns the template pattern. */
+  @Override public String toString() {
+    return format;
+  }
+
+  private Stream<String> parsePlaceholderValues(String input) {
+    return match(input)
+        .orElseThrow(
+            () -> new IllegalArgumentException("input doesn't match template (" + format + ")"))
+        .stream()
+        .map(Substring.Match::toString);
+  }
+
+  private static void extractLiteralsFromFormat(
+      String template, List<Substring.Match> placeholders,
+      List<String> literals, List<Substring.Pattern> literalLocators) {
+    int templateIndex = 0;
+    for (
+        int i = 0; i < placeholders.size();
+        templateIndex = placeholders.get(i).index() + placeholders.get(i).length(), i++) {
+      Substring.Match nextPlaceholder = placeholders.get(i);
+      int literalEnd = nextPlaceholder.index();
+      String literal = template.substring(templateIndex, literalEnd);
+      literals.add(literal);
+      if (i == 0) {
+        literalLocators.add(prefix(literal));  // First literal anchored to beginning
+        continue;
+      }
+      literalLocators.add(first(literal));  // Subsequent literals are searched
+      if (templateIndex >= literalEnd) {
+        throw new IllegalArgumentException(
+            "invalid pattern with '" + placeholders.get(i - 1) + nextPlaceholder + "'");
+      }
+    }
+    String literal = template.substring(templateIndex, template.length());
+    literals.add(literal);
+    // If no placeholder, anchor to beginning; else last literal anchored to end.
+    literalLocators.add(templateIndex == 0 ? prefix(literal) : suffix(literal));
+  }
+}
---
+++ mug/src/test/java/com/google/mu/util/StringFormatTest.java
@@ -0,0 +1,204 @@
+package com.google.mu.util;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.mu.util.Substring.first;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.testing.ClassSanityTester;
+import com.google.mu.util.stream.BiStream;
+import com.google.testing.junit.testparameterinjector.TestParameter;
+import com.google.testing.junit.testparameterinjector.TestParameterInjector;
+
+@RunWith(TestParameterInjector.class)
+public class StringFormatTest {
+
+  @Test public void parse_noPlaceholder() {
+    StringFormat template = new StringFormat("this is literal");
+    assertThat(template.parse("this is literal")).isEmpty();
+  }
+
+  @Test public void parse_onlyPlaceholder() {
+    StringFormat template = new StringFormat("%s");
+    assertThat(template.parse("Hello Tom!")).containsExactly("Hello Tom!");
+  }
+
+  @Test public void parse_singlePlaceholder() {
+    StringFormat template = new StringFormat("Hello %s!");
+    assertThat(template.parse("Hello Tom!")).containsExactly("Tom");
+  }
+
+  @Test public void parse_multiplePlaceholders() {
+    StringFormat template =
+        new StringFormat("Hello %s, welcome to %s!");
+    assertThat(template.parse("Hello Gandolf, welcome to Isengard!"))
+        .containsExactly("Gandolf", "Isengard")
+        .inOrder();
+  }
+
+  @Test public void parse_multiplePlaceholdersWithSameName() {
+    StringFormat template =
+        new StringFormat("Hello {name} and {name}!", "{", "}");
+    ImmutableListMultimap<String, String> result =
+        BiStream.zip(template.placeholders(), template.parse("Hello Gandolf and Aragon!"))
+            .mapKeys(Substring.Match::toString)
+            .collect(ImmutableListMultimap::toImmutableListMultimap);
+    assertThat(result)
+        .containsExactly("{name}", "Gandolf", "{name}", "Aragon")
+        .inOrder();
+  }
+
+  @Test public void parse_emptyPlaceholderValue() {
+    StringFormat template =
+        new StringFormat("Hello %s!");
+    assertThat(template.parse("Hello !")).containsExactly("");
+  }
+
+  @Test public void parse_preludeFailsToMatch() {
+    StringFormat template = new StringFormat("Hello %s!");
+    assertThrows(IllegalArgumentException.class, () -> template.parse("Hell Tom!"));
+    assertThrows(IllegalArgumentException.class, () -> template.parse("elloh Tom!"));
+    assertThrows(IllegalArgumentException.class, () -> template.parse(" Hello Tom!"));
+  }
+
+  @Test public void parse_postludeFailsToMatch() {
+    StringFormat template = new StringFormat("Hello %s!");
+    assertThrows(IllegalArgumentException.class, () -> template.parse("Hello Tom?"));
+    assertThrows(IllegalArgumentException.class, () -> template.parse("Hello Tom! "));
+    assertThrows(IllegalArgumentException.class, () -> template.parse("Hello Tom"));
+  }
+
+  @Test public void parse_nonEmptyTemplate_emptyInput() {
+    StringFormat template = new StringFormat("Hello %s!");
+    assertThrows(IllegalArgumentException.class, () -> template.parse(""));
+  }
+
+  @Test public void parse_emptyTemplate_nonEmptyInput() {
+    StringFormat template = new StringFormat("");
+    assertThrows(IllegalArgumentException.class, () -> template.parse("."));
+  }
+
+  @Test public void parse_emptyTemplate_emptyInput() {
+    StringFormat template = new StringFormat("");
+    assertThat(template.parse("")).isEmpty();
+  }
+
+  @Test public void parse_withOneArgLambda() {
+    assertThat(new StringFormat("1 is %s").parse("1 is one", Object::toString)).isEqualTo("one");
+  }
+
+  @Test public void parse_withTwoArgsLambda() {
+    assertThat(new StringFormat("1 is %s, 2 is %s").parse("1 is one, 2 is two", String::concat))
+        .isEqualTo("onetwo");
+  }
+
+  @Test public void parse_withThreeArgsLambda() {
+    String result =
+        new StringFormat("1 is %s, 2 is %s, 3 is %s")
+            .parse("1 is one, 2 is two, 3 is three", (x, y, z) -> x + "," + y + "," + z);
+    assertThat(result).isEqualTo("one,two,three");
+  }
+
+  @Test public void parse_withFourArgsLambda() {
+    String result =
+        new StringFormat("1 is %s, 2 is %s, 3 is %s, 4 is %s")
+            .parse("1 is one, 2 is two, 3 is three, 4 is four", (a, b, c, d) -> a + b + c + d);
+    assertThat(result).isEqualTo("onetwothreefour");
+  }
+
+  @Test public void parse_withFiveArgsLambda() {
+    String result =
+        new StringFormat("1 is %s, 2 is %s, 3 is %s, 4 is %s, 5 is %s")
+            .parse("1 is one, 2 is two, 3 is three, 4 is four, 5 is five", (a, b, c, d, e) -> a + b + c + d + e);
+    assertThat(result).isEqualTo("onetwothreefourfive");
+  }
+
+  @Test public void parse_withSixArgsLambda() {
+    String result =
+        new StringFormat("1 is %s, 2 is %s, 3 is %s, 4 is %s, 5 is %s, 6 is %s")
+            .parse("1 is one, 2 is two, 3 is three, 4 is four, 5 is five, 6 is six", (a, b, c, d, e, f) -> a + b + c + d + e + f);
+    assertThat(result).isEqualTo("onetwothreefourfivesix");
+  }
+
+  @Test public void parse_usingFormatString() {
+    assertThat(new StringFormat("My name is %s").parse("My name is one", Object::toString))
+        .isEqualTo("one");
+  }
+
+  @Test public void parse_ignoreTrailing() {
+    StringFormat template = new StringFormat("Hello {name}!{*}", "{", "}");
+    Map<String, String> result =
+        BiStream.zip(template.placeholders(), template.parse("Hello Tom! whatever"))
+            .mapKeys(Substring.Match::toString)
+            .skipKeysIf("{*}"::equals)
+            .toMap();
+    assertThat(result).containsExactly("{name}", "Tom");
+  }
+
+  @Test public void testFormat() {
+    assertThat(
+            new StringFormat("Dear %s, next stop is %s!").format("passengers", "Seattle"))
+        .isEqualTo("Dear passengers, next stop is Seattle!");
+    assertThat(
+            new StringFormat("Who is {person}", "{", "}").format("David"))
+        .isEqualTo("Who is David");
+  }
+
+  @Test public void testFormat_noArg() {
+    assertThat(new StringFormat("Hello world!").format()).isEqualTo("Hello world!");
+  }
+
+  @Test public void testFormat_nullArg() {
+    assertThat(
+            new StringFormat("Dear %s, next stop is %s!").format("passengers", null))
+        .isEqualTo("Dear passengers, next stop is null!");
+  }
+
+  @Test public void testFormat_fewerArgs() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new StringFormat("Dear %s, next stop is %s!").format("passengers"));
+  }
+
+  @Test public void testFormat_moreArgs() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new StringFormat("Dear %s, next stop is %s!").format("passengers", "Seattle", 1));
+  }
+
+  @Test public void testFormat_duplicatePlaceholderVariableNames() {
+    assertThat(new StringFormat("Hi {person} and {person}!", "{", "}").format("Tom", "Jerry"))
+        .isEqualTo("Hi Tom and Jerry!");
+  }
+
+  @Test public void testFormat_roundtrip(
+      @TestParameter({"", "k", ".", "foo"}) String key,
+      @TestParameter({"", "v", ".", "bar"}) String value) {
+    StringFormat template = new StringFormat("key : {key}, value : {value}", "{", "}");
+    String formatted = template.format(key, value);
+    assertThat(BiStream.zip(template.placeholders(), template.parse(formatted)).mapKeys(Substring.Match::toString).toMap())
+        .containsExactly("{key}", key, "{value}", value)
+        .inOrder();
+  }
+
+  @Test public void twoPlaceholdersNextToEachOther() {
+    assertThrows(IllegalArgumentException.class, () -> new StringFormat("{a}{b}", "{", "}"));
+    assertThrows(IllegalArgumentException.class, () -> new StringFormat("%s%s"));
+  }
+
+  @Test public void parse_partiallyOverlappingTemplate() {
+    assertThat(new StringFormat("xyz%sxzz").parse("xyzzxxzz"))
+        .containsExactly("zx");
+  }
+
+  @Test public void testNulls() {
+    new ClassSanityTester()
+        .setDefault(Substring.RepeatingPattern.class, first("%s").repeatedly())
+        .testNulls(StringFormat.class);
+  }
+}
