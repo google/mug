dfbdaf113532d60051c927891eeb0f81f227ac05
xingyutangyuan <147447743+xingyutangyuan@users.noreply.github.com>
2023-11-26 10:31:24 -0800

    Fixed mug-errorprone bazel BUILD files to make the checks effective.

--- mug-examples/src/test/java/com/google/mu/examples/HowToUseStringFormatTest.java
+++ mug-examples/src/test/java/com/google/mu/examples/HowToUseStringFormatTest.java
@@ -1,5 +1,7 @@
 package com.google.mu.examples;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -9,7 +11,11 @@ import com.google.mu.util.StringFormat;
 @RunWith(JUnit4.class)
 public class HowToUseStringFormatTest {
 
-  @Test public void testIt() {
+  @Test public void parseExample() {
+    StringFormat timeFormat = new StringFormat("{hour}:{minute}:{second}.{millis}");
+    String input = "12:10:01.234";
+    assertThat(timeFormat.parse(input, (hour, minute, second, millis) -> minute))
+        .isEqualTo("10");
   }
 
   @SuppressWarnings("StringUnformatArgsCheck")
