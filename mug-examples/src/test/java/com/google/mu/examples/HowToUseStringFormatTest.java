1664cc79a195d1ee35e531b1b00b9e2ae7fbf235
xingyutangyuan <147447743+xingyutangyuan@users.noreply.github.com>
2023-11-26 10:38:04 -0800

    Added more StringFormat usage examples.

--- mug-examples/src/test/java/com/google/mu/examples/HowToUseStringFormatTest.java
+++ mug-examples/src/test/java/com/google/mu/examples/HowToUseStringFormatTest.java
@@ -1,6 +1,6 @@
 package com.google.mu.examples;
 
-import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth8.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,11 +15,28 @@ public class HowToUseStringFormatTest {
     StringFormat timeFormat = new StringFormat("{hour}:{minute}:{second}.{millis}");
     String input = "12:10:01.234";
     assertThat(timeFormat.parse(input, (hour, minute, second, millis) -> minute))
-        .isEqualTo("10");
+        .hasValue("10");
+  }
+
+  @Test public void scanExample() {
+    StringFormat boldFormat = new StringFormat("<b>{bolded}</b>");
+    String input = "Please come back to the <b>front desk</b> at <b>12:00</b>.";
+    assertThat(boldFormat.scan(input, bolded -> bolded))
+        .containsExactly("front desk", "12:00");
   }
 
   @SuppressWarnings("StringUnformatArgsCheck")
   String failsBecauseTwoLambdaParametersAreExpected() {
 	  return new StringFormat("{key}:{value}").parseOrThrow("k:v", key -> key);
+  }
+
+  @SuppressWarnings("StringUnformatArgsCheck")
+  String failsBecauseLambdaParameterNamesAreOutOfOrder() {
+    return new StringFormat("{key}:{value}").parseOrThrow("k:v", (value, key) -> key);
+  }
+ 
+  @SuppressWarnings("StringFormatPlaceholderNamesCheck")
+  String failsDueToBadPlaceholderName() {
+    return new StringFormat("{?}:{-}").parseOrThrow("k:v", (k, v) -> k);
   }
 }
