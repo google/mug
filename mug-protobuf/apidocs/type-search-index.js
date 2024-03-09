d8ebc93ee01214fe85442120ab7127a8bb4fca10
Ben Yu <yujige@gmail.com>
2024-03-09 14:48:20 -0800

    mug-guava and mug-protobuf javadoc

---
+++ mug-protobuf/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.google.mu.protobuf.util","l":"MoreStructs"},{"p":"com.google.mu.protobuf.util","l":"MoreValues"},{"p":"com.google.mu.protobuf.util","l":"StructBuilder"},{"p":"com.google.mu.protobuf.util","l":"Structor"}]
