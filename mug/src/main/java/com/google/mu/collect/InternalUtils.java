6602758f14b40816b2060477a23ddb60703e53e8
Ben Yu <yujige@gmail.com>
2024-06-05 14:20:23 -0700

    Sequence class for efficient concatenatino

--- mug/src/main/java/com/google/mu/collect/InternalUtils.java
+++ mug/src/main/java/com/google/mu/collect/InternalUtils.java
@@ -1,9 +1,26 @@
+/*****************************************************************************
+ * ------------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License");           *
+ * you may not use this file except in compliance with the License.          *
+ * You may obtain a copy of the License at                                   *
+ *                                                                           *
+ * http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ *****************************************************************************/
 package com.google.mu.collect;
 
 import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toCollection;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collector;
@@ -14,6 +31,10 @@ final class InternalUtils {
     if (!condition) {
       throw new IllegalArgumentException(String.format(message, args));
     }
+  }
+  static <T> Collector<T, ?, List<T>> toImmutableList() {
+    return checkingNulls(
+        collectingAndThen(toCollection(ArrayList::new), Collections::unmodifiableList));
   }
 
   static <T> Collector<T, ?, Set<T>> toImmutableSet() {
---
+++ mug/src/main/java/com/google/mu/collect/Sequence.java
@@ -0,0 +1,157 @@
+/*****************************************************************************
+ * ------------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License");           *
+ * you may not use this file except in compliance with the License.          *
+ * You may obtain a copy of the License at                                   *
+ *                                                                           *
+ * http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ *****************************************************************************/
+package com.google.mu.collect;
+
+import static com.google.mu.collect.InternalUtils.toImmutableList;
+import static java.util.Objects.requireNonNull;
+
+import java.util.AbstractList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+import com.google.mu.util.graph.Walker;
+
+/**
+ * Immutable {@link List} implementation that supports O(1) concatenation.
+ *
+ * <p>The expected use case is to perform frequent concatenations using the {@code concat()}
+ * methods. O(n) materialization cost will be (lazily) paid before the first time accessing the
+ * elements either through the {@link List} methods such as {@link List#get}, {@link List#equals}
+ * or {@link #stream}, {@link #collect}.
+ *
+ * <p>On the other hand, it's inefficient to materialize and then concatenate (rinse and repeat).
+ *
+ * <p>Null elements are not allowed.
+ *
+ * @since 8.1
+ */
+public final class Sequence<T> extends AbstractList<T> {
+  private final T head;
+  private final Tree<T> tail;
+  private List<T> lazyElements;
+
+  /** Returns a Sequence with a single element. */
+  public static <T> Sequence<T> of(T element) {
+    return new Sequence<>(element, null);
+  }
+
+  /** Returns a Sequence with {@code first} followed by {@code remaining}. */
+  @SafeVarargs // remaining are copied into immutable object
+  public static <T> Sequence<T> of(T first, T... remaining) {
+    Tree<T> tail = null;
+    for (int i = remaining.length - 1; i >= 0; i--) {
+      tail = new Tree<>(remaining[i], null, tail);
+    }
+    return new Sequence<>(first, tail);
+  }
+
+  /** Returns a new Sequence concatenating {@code left} and {@code right} in <em>O(1)</em> time. */
+  public static <T> Sequence<T> concat(Sequence<? extends T> left, Sequence<? extends T> right) {
+    return new Sequence<>(left.head, new Tree<>(right.head, left.tail, right.tail));
+  }
+
+  /** Returns a new Sequence concatenating {@code this} and {@code lastElement} in <em>O(1)</em> time. */
+  public Sequence<T> concat(T lastElement) {
+    return concat(this, of(lastElement));
+  }
+
+  /** Returns the size of the sequence. This is an O(1) operation. */
+  @Override public int size() {
+    return 1 + sizeOf(tail);
+  }
+
+  /** Convenience method for {@code stream().collect(collector)}. */
+  public <R> R collect(Collector<T, ?, R> collector) {
+    return stream().collect(collector);
+  }
+
+  @Override
+  public Stream<T> stream() {
+    return elements().stream();
+  }
+
+  @Override public T get(int i) {
+    return elements().get(i);
+  }
+
+  @Override
+  public ListIterator<T> listIterator() {
+    return elements().listIterator();
+  }
+
+  @Override
+  public ListIterator<T> listIterator(int index) {
+    return elements().listIterator(index);
+  }
+
+  @Override
+  public List<T> subList(int fromIndex, int toIndex) {
+    return elements().subList(fromIndex, toIndex);
+  }
+
+  private Sequence(T head, Tree<T> tail) {
+    this.head = requireNonNull(head);
+    this.tail = tail;
+  }
+
+  // Visible for testing the idempotence
+  List<T> elements() {
+    List<T> elements = lazyElements;
+    if (elements == null) {
+      if (tail == null) {
+        elements = Collections.singletonList(head);
+      } else {
+        elements = Stream.concat(Stream.of(head), tail.stream()).collect(toImmutableList());
+      }
+      lazyElements = elements;
+    }
+    return elements;
+  }
+
+  private static final class Tree<T> {
+    private final T value;
+    private final Tree<? extends T> before;
+    private final Tree<? extends T> after;
+    private final int size;
+
+    Tree(T value, Tree<? extends T> before, Tree<? extends T> after) {
+      this.value = requireNonNull(value);
+      this.before = before;
+      this.after = after;
+      this.size = 1 + sizeOf(before) + sizeOf(after);
+    }
+
+    Stream<T> stream() {
+      return Walker.<Tree<T>>inBinaryTree(Tree::before, Tree::after).inOrderFrom(this).map(t -> t.value);
+    }
+
+    @SuppressWarnings("unchecked")  // Tree<T> is covariant (immutable)
+    Tree<T> before() {
+      return (Tree<T>) before;
+    }
+
+    @SuppressWarnings("unchecked")  // Tree<T> is covariant (immutable)
+    Tree<T> after() {
+      return (Tree<T>) after;
+    }
+  }
+
+  private static int sizeOf(Tree<?> tree) {
+    return tree == null ? 0 : tree.size;
+  }
+}
