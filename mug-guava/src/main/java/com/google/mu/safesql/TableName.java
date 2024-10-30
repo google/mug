ecffbc97b071dec6797c97aacc8a57a81584d782
Ben Yu <yujige@gmail.com>
2024-10-30 16:33:32 -0700

    Explain intention of @TableName

--- mug-guava/src/main/java/com/google/mu/safesql/TableName.java
+++ mug-guava/src/main/java/com/google/mu/safesql/TableName.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Retention;
 /**
  * Used to configure trusted table name to be used in safe SQL.
  *
+ * <p>Frameworks can use this annotation to configure table names on classes, methods,
+ * enum constants etc. at compile time.
+ *
  * @since 8.2
  */
 @Retention(RUNTIME)
