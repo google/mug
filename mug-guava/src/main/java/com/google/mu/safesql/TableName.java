1d8cccfad95c40da5894b4f06879a9cad5e1d414
Ben Yu <yujige@gmail.com>
2024-10-30 16:07:22 -0700

    of(TableName)

--- mug-guava/src/main/java/com/google/mu/safesql/TableName.java
+++ mug-guava/src/main/java/com/google/mu/safesql/TableName.java
@@ -4,7 +4,11 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 
-/** Used to configure trusted table name to be used in sql. */
+/**
+ * Used to configure trusted table name to be used in safe SQL.
+ *
+ * @since 8.2
+ */
 @Retention(RUNTIME)
 public @interface TableName {
   String value();
