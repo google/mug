d0100bddd3d93f4de6ea46ecf33c2381bbe7a052
Ben Yu <yujige@gmail.com>
2024-11-04 23:04:56 -0800

    rename mapper to rowMapper

--- mug-guava/src/main/java/com/google/mu/safesql/SafeSql.java
+++ mug-guava/src/main/java/com/google/mu/safesql/SafeSql.java
@@ -479,7 +479,7 @@ public final class SafeSql {
 
   /**
    * Executes the encapsulated SQL as a query against {@code connection}. The {@link ResultSet}
-   * will be iterated through, transformed by {@code mapper} and finally closed before returning.
+   * will be iterated through, transformed by {@code rowMapper} and finally closed before returning.
    *
    * <p>For example: <pre>{@code
    * List<String> names = SafeSql.of("SELECT name from Users where id = {id}", id)
@@ -489,12 +489,12 @@ public final class SafeSql {
    * @throws UncheckedSqlException wraps {@link SQLException} if failed
    */
   public <T> List<T> query(
-      Connection connection, SqlFunction<? super ResultSet, ? extends T> mapper) {
-    checkNotNull(mapper);
+      Connection connection, SqlFunction<? super ResultSet, ? extends T> rowMapper) {
+    checkNotNull(rowMapper);
     try {
       try (PreparedStatement stmt = prepareStatement(connection);
           ResultSet resultSet = stmt.executeQuery()) {
-        return mapResults(resultSet, mapper);
+        return mapResults(resultSet, rowMapper);
       }
     } catch (SQLException e) {
       throw new UncheckedSqlException(e);
@@ -555,9 +555,9 @@ public final class SafeSql {
   public static <T> Template<List<T>> prepareQuery(
       Connection connection,
       @CompileTimeConstant String template,
-      SqlFunction<? super ResultSet, ? extends T> mapper) {
+      SqlFunction<? super ResultSet, ? extends T> rowMapper) {
     checkNotNull(connection);
-    checkNotNull(mapper);
+    checkNotNull(rowMapper);
     Template<SafeSql> sqlTemplate = template(template);
     return new Template<List<T>>() {
       private PreparedStatement statement;
@@ -577,7 +577,7 @@ public final class SafeSql {
           }
           cachedSql = sql.toString();
           try (ResultSet resultSet = sql.setArgs(statement).executeQuery()) {
-            return mapResults(resultSet, mapper);
+            return mapResults(resultSet, rowMapper);
           }
         } catch (SQLException e) {
           throw new UncheckedSqlException(e);
