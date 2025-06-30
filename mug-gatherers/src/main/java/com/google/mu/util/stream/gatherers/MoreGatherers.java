94014d7ffe66fe0efb676f495d6c3b4e13fb0a77
Ben Yu <yujige@gmail.com>
2025-06-30 12:46:43 -0700

    MoreGatherers

---
+++ mug-gatherers/src/main/java/com/google/mu/util/stream/gatherers/MoreGatherers.java
@@ -0,0 +1,13 @@
+package com.google.mu.util.stream.gatherers;
+
+import java.util.function.Function;
+import java.util.stream.Gatherer;
+
+public final class MoreGatherers {
+
+  public <F, T> Gatherer<> concurrently(Function<? super F, ? extends T> mapper) {
+    return null;
+  }
+
+  private MoreGatherers() {}
+}
