cd5ba462460ba8b069015cbeef0da0d94e1eed3e
Ben Yu <yujige@gmail.com>
2025-07-27 09:17:21 +0800

    test multi-level exception for JdbcCloser

--- mug-safesql/src/test/java/com/google/mu/safesql/JdbcCloserTest.java
+++ mug-safesql/src/test/java/com/google/mu/safesql/JdbcCloserTest.java
@@ -1,11 +1,14 @@
 package com.google.mu.safesql;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.stream.Stream;
 
@@ -82,6 +85,30 @@ public class JdbcCloserTest {
       closer.register(statement::close);
       closer.register(resultSet::close);
     }
+    InOrder inOrder = Mockito.inOrder(connection, statement, resultSet);
+    inOrder.verify(resultSet).close();
+    inOrder.verify(statement).close();
+    inOrder.verify(connection).close();
+  }
+
+  @Test public void moreThanOneReousrces_allClosed_despiteException() throws Exception {
+    Connection connection = mock(Connection.class);
+    ResultSet resultSet = mock(ResultSet.class);
+    IllegalArgumentException connectionException = new IllegalArgumentException("connection");
+    SQLException resultSetException = new SQLException("result set");
+    Mockito.doThrow(connectionException).when(connection).close();
+    Mockito.doThrow(resultSetException).when(resultSet).close();
+    Exception thrown = assertThrows(
+        UncheckedSqlException.class,
+        () -> {
+          try (JdbcCloser closer = new JdbcCloser()) {
+            closer.register(connection::close);
+            closer.register(statement::close);
+            closer.register(resultSet::close);
+          }
+        });
+    assertThat(thrown).hasCauseThat().isSameInstanceAs(resultSetException);
+    assertThat(thrown.getCause().getSuppressed()).asList().containsExactly(connectionException);
     InOrder inOrder = Mockito.inOrder(connection, statement, resultSet);
     inOrder.verify(resultSet).close();
     inOrder.verify(statement).close();
