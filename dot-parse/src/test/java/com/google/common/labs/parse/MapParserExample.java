6394d27aba026fe24b1e39b3065ea38c4adb31e9
Ben Yu <yujige@gmail.com>
2025-10-09 21:06:20 -0700

    MapParserExample

--- dot-parse/src/test/java/com/google/common/labs/parse/MapParserExample.java
+++ dot-parse/src/test/java/com/google/common/labs/parse/MapParserExample.java
@@ -30,11 +30,15 @@ public class MapParserExample {
   @Test public void testWithNestedList() {
     assertThat(parse("{k1 : v1, k2 : [v2, v3]}"))
         .isEqualTo(Map.of("k1", "v1", "k2", List.of("v2", "v3")));
+    assertThat(parse("{k1 : v1, k2 : []}"))
+        .isEqualTo(Map.of("k1", "v1", "k2", List.of()));
   }
 
   @Test public void testWithNestedMap() {
     assertThat(parse("{k1 : v1, k2 : {k21 : v21, k22 : v22}}"))
         .isEqualTo(Map.of("k1", "v1", "k2", Map.of("k21", "v21", "k22", "v22")));
+    assertThat(parse("{k1 : v1, k2 : {}}"))
+        .isEqualTo(Map.of("k1", "v1", "k2", Map.of()));
   }
 
   static Map<String, ?> parse(String input) {
