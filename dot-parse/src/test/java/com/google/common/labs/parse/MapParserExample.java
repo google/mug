4f8354f5e1d42163670711c6e539fe9819575069
Ben Yu <yujige@gmail.com>
2025-10-09 20:58:04 -0700

    MapParserExample

---
+++ dot-parse/src/test/java/com/google/common/labs/parse/MapParserExample.java
@@ -0,0 +1,49 @@
+package com.google.common.labs.parse;
+
+import static com.google.common.labs.parse.Parser.anyOf;
+import static com.google.common.labs.parse.Parser.string;
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.google.mu.util.CharPredicate;
+import com.google.mu.util.stream.BiStream;
+
+@RunWith(JUnit4.class)
+public class MapParserExample {
+
+  @Test public void testEmptyMap() {
+    assertThat(parse("{}")).isEqualTo(Map.of());
+  }
+
+  @Test public void testSimpleKeyValues() {
+    assertThat(parse("{k1 : v1, k2 : v2}")).isEqualTo(Map.of("k1", "v1", "k2", "v2"));
+  }
+
+  @Test public void testWithNestedList() {
+    assertThat(parse("{k1 : v1, k2 : [v2, v3]}"))
+        .isEqualTo(Map.of("k1", "v1", "k2", List.of("v2", "v3")));
+  }
+
+  @Test public void testWithNestedMap() {
+    assertThat(parse("{k1 : v1, k2 : {k21 : v21, k22 : v22}}"))
+        .isEqualTo(Map.of("k1", "v1", "k2", Map.of("k21", "v21", "k22", "v22")));
+  }
+
+  static Map<String, ?> parse(String input) {
+    Parser<String> word = Parser.consecutive(CharPredicate.WORD, "key");
+    Parser.Lazy<Map<String, ?>> lazy = new Parser.Lazy<>();
+    Parser<List<String>> listParser = word.zeroOrMoreDelimitedBy(",").between("[", "]");
+    Parser<?> valueParser = anyOf(word, listParser, lazy);
+    Parser<Map<String, ?>> mapParser = Parser.sequence(word, string(":").then(valueParser), Map::entry)
+        .zeroOrMoreDelimitedBy(",")
+        .between("{", "}")
+        .map(entries -> BiStream.from(entries, Map.Entry::getKey, Map.Entry::getValue).toMap());
+    return lazy.delegateTo(mapParser).parseSkipping(Character::isWhitespace, input);
+  }
+}
