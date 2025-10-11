563dcae972fe405f5766d760c8a05a4825031b07
Ben Yu <yujige@gmail.com>
2025-10-11 12:38:21 -0700

    add comment to Csv

--- dot-parse/src/main/java/com/google/common/labs/csv/Csv.java
+++ dot-parse/src/main/java/com/google/common/labs/csv/Csv.java
@@ -112,7 +112,7 @@ public final class Csv {
     Parser<String> unquoted = consecutive(UNRESERVED_CHAR.and(is(delim).not()), "unquoted field");
     Parser<R> line =
         Parser.anyOf(
-            NEW_LINE.map(unused -> finisher.apply(supplier.get())),
+            NEW_LINE.map(unused -> finisher.apply(supplier.get())),  // empty line => [], not [""]
             QUOTED.or(unquoted)
                 .orElse("")
                 .delimitedBy(String.valueOf(delim), rowCollector)
