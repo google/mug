2083229fdf407015e9bfd4b6ff3ef0ac62018089
Ben Yu <yujige@gmail.com>
2025-10-25 17:23:12 -0700

    fix javadoc

--- dot-parse/src/main/java/com/google/common/labs/ini/IniConfig.java
+++ dot-parse/src/main/java/com/google/common/labs/ini/IniConfig.java
@@ -70,7 +70,7 @@ import com.google.mu.util.stream.BiStream;
  *
  * <p>TOML list of tables is supported by using multiple sections of the same name
  * (i.e. {@code [plugin]}, not {@code [[plugins]]}),
- * and you can access these sections using the {@link #getSections} method.
+ * and you can access these sections using the {@link #sections(String)} method.
  */
 @Immutable
 @SuppressWarnings("Immutable") // Map is immutable
