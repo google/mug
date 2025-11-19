466a80aaed2f60a750c1fb492c51ac3016fb9c55
Ben Yu <yujige@gmail.com>
2025-11-19 07:29:23 -0800

    rename find() to first() because find() and probe() are too similar

--- dot-parse/src/main/java/com/google/common/labs/parse/Parser.java
+++ dot-parse/src/main/java/com/google/common/labs/parse/Parser.java
@@ -186,8 +186,8 @@ public abstract class Parser<T> {
   }
 
   /**
-   * Returns a parser that finds the literal {@code string} that may start from the current position
-   * or after any number of characters.
+   * Returns a parser that finds the first literal {@code string} that may start from the current
+   * position or after any number of characters.
    *
    * <p>Useful when you need to skip characters until a particular anchor point, something awkward
    * to express in regex.
@@ -198,7 +198,7 @@ public abstract class Parser<T> {
    * backtracking because {@code [^>]+} can eat up the "dir..." characters so the regex engine would
    * have to back track in order to match those later patterns.
    *
-   * <p>With the {@code find()} parser, such pattern can be expressed more straight-forwardly, and
+   * <p>With the {@code first()} parser, such pattern can be expressed more straight-forwardly, and
    * not subject to backtracking:
    *
    * <pre>{@code
@@ -206,7 +206,7 @@ public abstract class Parser<T> {
    *
    * Substring.between("<div", ">").from(html)               // locate the div tag
    *     .flatMap(tag ->
-   *         Parser.find("dir")                              // find the "dir" inside the tag
+   *         Parser.first("dir")                             // find the "dir" inside the tag
    *             .followedBy("=")
    *             .then(word().immediatelyBetween("'", "'"))  // ltr or rtl
    *             .skipping(whitespace())                     // ignore whitespaces
@@ -216,12 +216,12 @@ public abstract class Parser<T> {
    *
    * @since 9.5
    */
-  public static Parser<String> find(String value) {
+  public static Parser<String> first(String value) {
     checkArgument(value.length() > 0, "value cannot be empty");
     return new Parser<>() {
       @Override MatchResult<String> skipAndMatch(
           Parser<?> skip, CharInput input, int start, ErrorContext context) {
-        // Unlike other parsers, find() doesn't apply the skip parser first. Its job is to find the
+        // Unlike other parsers, first() doesn't apply the skip parser first. Its job is to find the
         // value string, and the characters it skips are simply non-matching characters. Applying
         // skip could cause the match to fail if value itself contains characters that
         // would be skipped (e.g. whitespace).
