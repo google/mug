ee2fa4b3a25affbcea5786050f106ed0872422b5
Ben Yu <yujige@gmail.com>
2025-11-26 09:37:25 -0800

    refine javadoc for quotedBy()

--- dot-parse/src/main/java/com/google/common/labs/parse/Parser.java
+++ dot-parse/src/main/java/com/google/common/labs/parse/Parser.java
@@ -235,8 +235,8 @@ public abstract class Parser<T> {
   }
 
   /**
-   * Matches the range between {@code before} and {@code after}, inclusive, and returns the string
-   * in between. For example: {@code quotedBy('"', '"').parse("\"foo\"")} will return {@code "foo"}.
+   * Matches the characters quoted by {@code before} and {@code after}, and returns the string in
+   * between. For example: {@code quotedBy('"', '"').parse("\"foo\"")} will return {@code "foo"}.
    *
    * <p>If you need to support backslash escapes, use {@link #quotedStringWithEscapes} instead.
    *
@@ -247,8 +247,8 @@ public abstract class Parser<T> {
   }
 
   /**
-   * Matches the range between {@code before} and {@code after}, inclusive, and returns the string
-   * in between. For example: {@code quotedBy("'", "'").parse("'foo'")} will return {@code "foo"}.
+   * Matches the characters quoted by {@code before} and {@code after}, and returns the string in
+   * between. For example: {@code quotedBy("'", "'").parse("'foo'")} will return {@code "foo"}.
    *
    * <p>If you need to support backslash escapes, use {@link #quotedStringWithEscapes} instead.
    *
