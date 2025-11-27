ba458612d653dfa61a727d1823f00d6b34493864
Ben Yu <yujige@gmail.com>
2025-11-26 22:00:39 -0800

    use xml comment as example for quotedBy(String, String)

--- dot-parse/src/main/java/com/google/common/labs/parse/Parser.java
+++ dot-parse/src/main/java/com/google/common/labs/parse/Parser.java
@@ -263,7 +263,8 @@ public abstract class Parser<T> {
 
   /**
    * Matches the characters quoted by {@code before} and {@code after}, and returns the string in
-   * between. For example: {@code quotedBy("'", "'").parse("'foo'")} will return {@code "foo"}.
+   * between. For example: {@code quotedBy("<!--", "-->").parse("<!--comment-->")} will return
+   * {@code "comment"}.
    *
    * <p>If you need to support backslash escapes, use {@link #quotedStringWithEscapes} instead.
    *
