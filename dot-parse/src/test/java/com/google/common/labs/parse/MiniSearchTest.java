23c1dd36c430f0ecf804092d4cac1ec614ffd5aa
Ben Yu <yujige@gmail.com>
2025-12-04 19:21:35 -0800

    quotedByWithEscapes

--- dot-parse/src/test/java/com/google/common/labs/parse/MiniSearchTest.java
+++ dot-parse/src/test/java/com/google/common/labs/parse/MiniSearchTest.java
@@ -67,7 +67,7 @@ public class MiniSearchTest {
       // A search term is either quoted, or unquoted (but cannot be a keyword)
       Parser<Term> unquoted =
           word().suchThat(w -> !keywords.contains(w), "search term").map(Term::new);
-      Parser<Term> quoted = Parser.quotedStringWithEscapes('"', Parser.chars(1)).map(Term::new);
+      Parser<Term> quoted = Parser.quotedByWithEscapes('"', '"', (Parser<? extends CharSequence>) Parser.chars(1)).map(Term::new);
 
       // Leaf-level search term can be a quoted, unquoted term, or a sub-criteria inside parentheses.
       // They are then grouped by the boolean operators.
