89902f3763b35d6754650b1eb619e3dcae0c6cb5
Ben Yu <yujige@gmail.com>
2026-03-13 23:28:14 -0700

    update versions

--- mug/src/main/java/com/google/mu/annotations/ParametersMustMatchByName.java
+++ mug/src/main/java/com/google/mu/annotations/ParametersMustMatchByName.java
@@ -89,7 +89,7 @@ import java.lang.annotation.Target;
  *             <path>
  *               <groupId>com.google.mug</groupId>
  *               <artifactId>mug-errorprone</artifactId>
- *               <version>9.9.4</version>
+ *               <version>9.9.6</version>
  *             </path>
  *           </annotationProcessorPaths>
  *         </configuration>
