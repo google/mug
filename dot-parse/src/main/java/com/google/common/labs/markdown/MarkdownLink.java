d10ec9f0483bb9d6d58934d79c224373fd76eb19
Ben Yu <yujige@gmail.com>
2026-06-02 21:54:26 -0700

    discuss escaping and nesting for MarkdownLink

--- dot-parse/src/main/java/com/google/common/labs/markdown/MarkdownLink.java
+++ dot-parse/src/main/java/com/google/common/labs/markdown/MarkdownLink.java
@@ -45,9 +45,10 @@ import com.google.common.labs.parse.Parser;
  *     .scan(markdown, MarkdownLink::new);
  * }</pre>
  *
- * The parser properly handles escaping inside and outside of the link, and won't mistakenly
- * extract link-like syntax from backtick-quoted code or code blocks (recognizing code blocks
- * quoted by single backtick, double, triple or any number of consecutive backticks).
+ * The parser properly handles escaping inside and outside of the link; nesting within the link
+ * label and link url; and won't mistakenly extract link-like syntax from backtick-quoted code
+ * or code blocks (recognizing code blocks quoted by single backtick, double, triple or any number
+ * of consecutive backticks).
  *
  * @since 10.3
  */
