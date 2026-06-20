eddc4400af4bb534bd861242c3874bea20c519d7
Ben Yu <yujige@gmail.com>
2026-06-20 15:04:19 -0700

    parboiled2 and spc benchmark code

---
+++ mug-benchmarks/src/test/scala/com/google/mu/benchmarks/parsers/parboiled2/Parboiled2Showdown.scala
@@ -0,0 +1,178 @@
+package com.google.mu.benchmarks.parsers.parboiled2
+
+import com.google.mu.benchmarks.parsers.BenchmarkInputs
+import org.parboiled2._
+import scala.util.{Success, Failure}
+import scala.jdk.CollectionConverters._
+
+object Parboiled2Showdown {
+
+  // 1. IP Address
+  class IpParser(val input: ParserInput) extends Parser {
+    def digit = rule { oneOrMore(CharPredicate.Digit) }
+    def dot = rule { '.' }
+    def ip = rule { digit ~ dot ~ digit ~ dot ~ digit ~ dot ~ digit ~ EOI }
+  }
+
+  object IpFixture {
+    // Verify
+    new IpParser(BenchmarkInputs.IP).ip.run() match {
+      case Success(_) =>
+      case Failure(err) => throw new AssertionError(s"parboiled2 Ip verification failed: $err")
+    }
+  }
+
+  class IpFixture {
+    def run(): Any = {
+      new IpParser(BenchmarkInputs.IP).ip.run()
+    }
+  }
+
+  // 2. Quoted String
+  class StringParser(val input: ParserInput) extends Parser {
+    def esc = rule { '\\' ~ ANY }
+    def normal = rule { !CharPredicate('"', '\\') ~ ANY }
+    def quotedString = rule { '"' ~ zeroOrMore(esc | normal) ~ '"' ~ EOI }
+  }
+
+  object StringFixture {
+    // Verify
+    new StringParser(BenchmarkInputs.STRING_SIMPLE).quotedString.run() match {
+      case Success(_) =>
+      case Failure(err) => throw new AssertionError(s"parboiled2 StringSimple verification failed: $err")
+    }
+    new StringParser(BenchmarkInputs.STRING_ESCAPED).quotedString.run() match {
+      case Success(_) =>
+      case Failure(err) => throw new AssertionError(s"parboiled2 StringEscaped verification failed: $err")
+    }
+  }
+
+  class StringFixture {
+    def run(input: String): Any = {
+      new StringParser(input).quotedString.run()
+    }
+  }
+
+  // 3. Keywords (Case-Sensitive)
+  class KeywordsParser(val input: ParserInput) extends Parser {
+    def keywords = rule {
+      ( str("select")
+      | str("insert")
+      | str("update")
+      | str("delete")
+      | str("create")
+      | str("drop")
+      | str("alter")
+      | str("where")
+      | str("group")
+      | str("order")
+      | str("having")
+      | str("limit")
+      ) ~ EOI
+    }
+  }
+
+  object KeywordsFixture {
+    // Verify
+    for (keyword <- BenchmarkInputs.KEYWORDS.asScala) {
+      new KeywordsParser(keyword).keywords.run() match {
+        case Success(_) =>
+        case Failure(err) => throw new AssertionError(s"parboiled2 Keywords verification failed for $keyword: $err")
+      }
+    }
+  }
+
+  class KeywordsFixture {
+    def run(input: String): Any = {
+      new KeywordsParser(input).keywords.run()
+    }
+  }
+
+  // 4. Keywords (Case-Insensitive)
+  class IgnoreCaseParser(val input: ParserInput) extends Parser {
+    def keywords = rule {
+      ( ignoreCase("select")
+      | ignoreCase("insert")
+      | ignoreCase("update")
+      | ignoreCase("delete")
+      | ignoreCase("create")
+      | ignoreCase("drop")
+      | ignoreCase("alter")
+      | ignoreCase("where")
+      | ignoreCase("group")
+      | ignoreCase("order")
+      | ignoreCase("having")
+      | ignoreCase("limit")
+      ) ~ EOI
+    }
+  }
+
+  object IgnoreCaseFixture {
+    // Verify
+    for (keyword <- BenchmarkInputs.KEYWORDS.asScala) {
+      new IgnoreCaseParser(keyword.toUpperCase).keywords.run() match {
+        case Success(_) =>
+        case Failure(err) => throw new AssertionError(s"parboiled2 IgnoreCase verification failed for $keyword: $err")
+      }
+    }
+  }
+
+  class IgnoreCaseFixture {
+    def run(input: String): Any = {
+      new IgnoreCaseParser(input).keywords.run()
+    }
+  }
+
+  // 5. Calculator
+  class CalculatorParser(val input: ParserInput) extends Parser {
+    def ws = rule { zeroOrMore(anyOf(" \t\r\n")) }
+    def number = rule { capture(optional('-') ~ oneOrMore(CharPredicate.Digit)) ~> (_.toInt) ~ ws }
+    def atom: Rule1[Int] = rule { number | '(' ~ ws ~ expr ~ ')' ~ ws }
+    def term: Rule1[Int] = rule { atom ~ zeroOrMore(
+      '*' ~ ws ~ atom ~> ((a: Int, b: Int) => a * b)
+      | '/' ~ ws ~ atom ~> ((a: Int, b: Int) => a / b)
+    ) }
+    def expr: Rule1[Int] = rule { term ~ zeroOrMore(
+      '+' ~ ws ~ term ~> ((a: Int, b: Int) => a + b)
+      | '-' ~ ws ~ term ~> ((a: Int, b: Int) => a - b)
+    ) }
+    def root = rule { ws ~ expr ~ EOI }
+  }
+
+  object CalculatorFixture {
+    // Verify
+    new CalculatorParser(BenchmarkInputs.CALCULATOR).root.run() match {
+      case Success(res) =>
+        if (res != BenchmarkInputs.CALCULATOR_EXPECTED) {
+          throw new AssertionError(s"parboiled2 Calculator expected ${BenchmarkInputs.CALCULATOR_EXPECTED} but got $res")
+        }
+      case Failure(err) => throw new AssertionError(s"parboiled2 Calculator verification failed: $err")
+    }
+  }
+
+  class CalculatorFixture {
+    def run(): Any = {
+      new CalculatorParser(BenchmarkInputs.CALCULATOR).root.run()
+    }
+  }
+
+  // 6. Nested Comment
+  class NestedCommentParser(val input: ParserInput) extends Parser {
+    def comment: Rule0 = rule { "/*" ~ zeroOrMore(comment | !"*/" ~ ANY) ~ "*/" }
+    def root = rule { comment ~ EOI }
+  }
+
+  object NestedCommentFixture {
+    // Verify
+    new NestedCommentParser(BenchmarkInputs.NESTED_COMMENT).root.run() match {
+      case Success(_) =>
+      case Failure(err) => throw new AssertionError(s"parboiled2 NestedComment verification failed: $err")
+    }
+  }
+
+  class NestedCommentFixture {
+    def run(): Any = {
+      new NestedCommentParser(BenchmarkInputs.NESTED_COMMENT).root.run()
+    }
+  }
+}
---
+++ mug-benchmarks/src/test/scala/com/google/mu/benchmarks/parsers/scalaparser/ScalaParserShowdown.scala
@@ -0,0 +1,144 @@
+package com.google.mu.benchmarks.parsers.scalaparser
+
+import com.google.mu.benchmarks.parsers.BenchmarkInputs
+import scala.util.parsing.combinator.RegexParsers
+import scala.jdk.CollectionConverters._
+
+object ScalaParserShowdown {
+  
+  object IpFixture extends RegexParsers {
+    override val skipWhitespace = false
+    private val digit = """[0-9]+""".r
+    private val dot = "."
+    val parser = (digit ~ dot ~ digit ~ dot ~ digit ~ dot ~ digit) ^^ { _ => "ip" }
+    
+    // Verify
+    parse(parser, BenchmarkInputs.IP) match {
+      case Success(_, next) if next.atEnd =>
+      case other => throw new AssertionError(s"scala-parser-combinators Ip verification failed: $other")
+    }
+  }
+
+  class IpFixture {
+    def run(): Any = {
+      IpFixture.parse(IpFixture.parser, BenchmarkInputs.IP)
+    }
+  }
+
+  object StringFixture extends RegexParsers {
+    override val skipWhitespace = false
+    private val stringVal = "\"" + """([^"\\]|\\.)*""" + "\""
+    val parser = stringVal.r
+
+    // Verify
+    parse(parser, BenchmarkInputs.STRING_SIMPLE) match {
+      case Success(_, next) if next.atEnd =>
+      case other => throw new AssertionError(s"scala-parser-combinators StringSimple verification failed: $other")
+    }
+    parse(parser, BenchmarkInputs.STRING_ESCAPED) match {
+      case Success(_, next) if next.atEnd =>
+      case other => throw new AssertionError(s"scala-parser-combinators StringEscaped verification failed: $other")
+    }
+  }
+
+  class StringFixture {
+    def run(input: String): Any = {
+      StringFixture.parse(StringFixture.parser, input)
+    }
+  }
+
+  object KeywordsFixture extends RegexParsers {
+    override val skipWhitespace = false
+    val parser = BenchmarkInputs.KEYWORDS.asScala.mkString("|").r
+
+    // Verify
+    for (keyword <- BenchmarkInputs.KEYWORDS.asScala) {
+      parse(parser, keyword) match {
+        case Success(_, next) if next.atEnd =>
+        case other => throw new AssertionError(s"scala-parser-combinators Keywords verification failed for $keyword: $other")
+      }
+    }
+  }
+
+  class KeywordsFixture {
+    def run(input: String): Any = {
+      KeywordsFixture.parse(KeywordsFixture.parser, input)
+    }
+  }
+
+  object IgnoreCaseFixture extends RegexParsers {
+    override val skipWhitespace = false
+    val parser = ("(?i)" + BenchmarkInputs.KEYWORDS.asScala.mkString("|")).r
+
+    // Verify
+    for (keyword <- BenchmarkInputs.KEYWORDS.asScala) {
+      parse(parser, keyword.toUpperCase) match {
+        case Success(_, next) if next.atEnd =>
+        case other => throw new AssertionError(s"scala-parser-combinators IgnoreCase verification failed for $keyword: $other")
+      }
+    }
+  }
+
+  class IgnoreCaseFixture {
+    def run(input: String): Any = {
+      IgnoreCaseFixture.parse(IgnoreCaseFixture.parser, input)
+    }
+  }
+
+  object CalculatorFixture extends RegexParsers {
+    private val number = """-?[0-9]+""".r ^^ { _.toInt }
+    
+    private def atom: Parser[Int] = number | "(" ~> expr <~ ")"
+    
+    private def term: Parser[Int] = atom ~ rep("*" ~ atom | "/" ~ atom) ^^ {
+      case first ~ list => list.foldLeft(first) {
+        case (acc, "*" ~ x) => acc * x
+        case (acc, "/" ~ x) => acc / x
+      }
+    }
+    
+    private def expr: Parser[Int] = term ~ rep("+" ~ term | "-" ~ term) ^^ {
+      case first ~ list => list.foldLeft(first) {
+        case (acc, "+" ~ x) => acc + x
+        case (acc, "-" ~ x) => acc - x
+      }
+    }
+    
+    val parser = expr
+
+    // Verify
+    parse(parser, BenchmarkInputs.CALCULATOR) match {
+      case Success(res, next) if next.atEnd =>
+        if (res != BenchmarkInputs.CALCULATOR_EXPECTED) {
+          throw new AssertionError(s"scala-parser-combinators Calculator expected ${BenchmarkInputs.CALCULATOR_EXPECTED} but got $res")
+        }
+      case other => throw new AssertionError(s"scala-parser-combinators Calculator verification failed: $other")
+    }
+  }
+
+  class CalculatorFixture {
+    def run(): Any = {
+      CalculatorFixture.parse(CalculatorFixture.parser, BenchmarkInputs.CALCULATOR)
+    }
+  }
+
+  object NestedCommentFixture extends RegexParsers {
+    override val skipWhitespace = false
+    
+    private def comment: Parser[Unit] = "/*" ~ rep(comment | not("*/") ~> ".".r) ~ "*/" ^^ { _ => () }
+    
+    val parser = comment
+
+    // Verify
+    parse(parser, BenchmarkInputs.NESTED_COMMENT) match {
+      case Success(_, next) if next.atEnd =>
+      case other => throw new AssertionError(s"scala-parser-combinators NestedComment verification failed: $other")
+    }
+  }
+
+  class NestedCommentFixture {
+    def run(): Any = {
+      NestedCommentFixture.parse(NestedCommentFixture.parser, BenchmarkInputs.NESTED_COMMENT)
+    }
+  }
+}
