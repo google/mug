4f77bacbd7300d8d630296edb7443b0e9ec48aa3
Ben Yu <yujige@gmail.com>
2026-09-01 14:22:36 -0700

    PrecomputedForAscii

--- mug/src/test/java/com/google/mu/util/PrecomputedForAsciiTest.java
+++ mug/src/test/java/com/google/mu/util/PrecomputedForAsciiTest.java
@@ -26,7 +26,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class PrecomputedCharPredicateTest {
+public class PrecomputedForAsciiTest {
 
   @Test public void precomputeForAscii_none() {
     CharPredicate predicate = CharPredicate.NONE.precomputeForAscii();
